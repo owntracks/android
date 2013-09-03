e7418c9d2970342f52bf35d8d43f41b1ba94cd7f
Alexander Rust <mail@alr.st>
2013-09-03 21:05:17 +0200

    fixed #92

--- src/st/alr/mqttitude/support/Locator.java
+++ src/st/alr/mqttitude/support/Locator.java
@@ -80,7 +80,7 @@ public abstract class Locator implements MqttPublish {
         context.startService(service);
 
         payload.append("{");
-        payload.append("\"type_\": ").append("\"").append("location").append("\"");
+        payload.append("\"_type\": ").append("\"").append("location").append("\"");
         payload.append(", \"lat\": ").append("\"").append(l.getLatitude()).append("\"");
         payload.append(", \"lon\": ").append("\"").append(l.getLongitude()).append("\"");
         payload.append(", \"tst\": ").append("\"").append((int)(d.getTime()/1000)).append("\"");
