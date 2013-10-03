b12070172514cd223adf3f784b674ac1494f21b3
Alexander Rust <mail@alr.st>
2013-10-03 16:50:45 +0200

    re-added project files

--- gen/st/alr/mqttitude/R.java
+++ gen/st/alr/mqttitude/R.java
@@ -9,18 +9,18 @@ package st.alr.mqttitude;
 
 public final class R {
     public static final class array {
-        public static final int connect_options_security=0x7f040000;
-        public static final int qos=0x7f040001;
-        public static final int qos_readable=0x7f040002;
+        public static final int connect_options_security=0x7f060000;
+        public static final int qos=0x7f060001;
+        public static final int qos_readable=0x7f060002;
     }
     public static final class attr {
     }
     public static final class color {
-        public static final int background=0x7f060003;
-        public static final int border=0x7f060004;
-        public static final int darkGrey=0x7f060002;
-        public static final int lightGrey=0x7f060000;
-        public static final int mediumGrey=0x7f060001;
+        public static final int background=0x7f080003;
+        public static final int border=0x7f080004;
+        public static final int darkGrey=0x7f080002;
+        public static final int lightGrey=0x7f080000;
+        public static final int mediumGrey=0x7f080001;
     }
     public static final class drawable {
         public static final int ab_bottom_solid=0x7f020000;
@@ -32,66 +32,121 @@ public final class R {
         public static final int ic_share=0x7f020006;
         public static final int ic_upload=0x7f020007;
     }
+    public static final class id {
+        public static final int brokerClientId=0x7f0a000f;
+        public static final int brokerError=0x7f0a0009;
+        public static final int brokerHost=0x7f0a000b;
+        public static final int brokerPassword=0x7f0a0013;
+        public static final int brokerPort=0x7f0a000d;
+        public static final int brokerSecurity=0x7f0a0015;
+        public static final int brokerSecurityNoneOptions=0x7f0a0019;
+        public static final int brokerSecuritySSLCaCrtPath=0x7f0a0018;
+        public static final int brokerSecuritySSLOptions=0x7f0a0016;
+        public static final int brokerStatus=0x7f0a0008;
+        public static final int brokerUsername=0x7f0a0011;
+        public static final int bugsnagHeader=0x7f0a001f;
+        public static final int bugsnagLicense=0x7f0a0020;
+        public static final int eventbusHeader=0x7f0a001b;
+        public static final int eventbusLicense=0x7f0a001c;
+        public static final int locatorCurAccuracy=0x7f0a0003;
+        public static final int locatorCurLatLon=0x7f0a0002;
+        public static final int locatorCurLatLonTime=0x7f0a0004;
+        public static final int locatorLastPubAccuracy=0x7f0a0006;
+        public static final int locatorLastPubLatLon=0x7f0a0005;
+        public static final int locatorLastPubLatLonTime=0x7f0a0007;
+        public static final int locatorStatus=0x7f0a0001;
+        public static final int menu_publish=0x7f0a0021;
+        public static final int menu_settings=0x7f0a0024;
+        public static final int menu_share=0x7f0a0022;
+        public static final int menu_status=0x7f0a0023;
+        public static final int pager=0x7f0a0000;
+        public static final int pahoHeader=0x7f0a001d;
+        public static final int pahoLicense=0x7f0a001e;
+        public static final int scrollView1=0x7f0a001a;
+        public static final int textView1=0x7f0a000a;
+        public static final int textView2=0x7f0a000c;
+        public static final int textView3=0x7f0a0010;
+        public static final int textView4=0x7f0a0012;
+        public static final int textView5=0x7f0a0014;
+        public static final int textView6=0x7f0a0017;
+        public static final int textView8=0x7f0a000e;
+    }
+    public static final class layout {
+        public static final int activity_main=0x7f030000;
+        public static final int activity_status=0x7f030001;
+        public static final int fragment_friends=0x7f030002;
+        public static final int fragment_status=0x7f030003;
+        public static final int preferences_broker=0x7f030004;
+        public static final int preferences_licenses=0x7f030005;
+    }
+    public static final class menu {
+        public static final int activity_main=0x7f090000;
+        public static final int activity_preferences=0x7f090001;
+        public static final int activity_status=0x7f090002;
+    }
     public static final class string {
-        public static final int action_settings=0x7f030001;
-        public static final int app_name=0x7f030000;
-        public static final int cancel=0x7f030032;
-        public static final int connectivityConnected=0x7f030029;
-        public static final int connectivityConnecting=0x7f030028;
-        public static final int connectivityDisconnected=0x7f03002b;
-        public static final int connectivityDisconnectedDataDisabled=0x7f03002d;
-        public static final int connectivityDisconnectedUserDisconnect=0x7f03002c;
-        public static final int connectivityDisconnecting=0x7f03002a;
-        public static final int currentLocation=0x7f03002f;
-        public static final int error=0x7f030033;
-        public static final int na=0x7f03002e;
-        public static final int preferencesActivity=0x7f03001e;
-        public static final int preferencesBackgroundUpdatesInterval=0x7f03001f;
-        public static final int preferencesBackgroundUpdatesIntervalSummary=0x7f030020;
-        public static final int preferencesBackgroundUpdatesSummary=0x7f030022;
-        public static final int preferencesBackroundUpdates=0x7f030021;
-        public static final int preferencesBrokerClientId=0x7f030006;
-        public static final int preferencesInfo=0x7f030023;
-        public static final int preferencesLicensesDismiss=0x7f03000c;
-        public static final int preferencesLicensesSummary=0x7f03000b;
-        public static final int preferencesLicensesTitle=0x7f03000a;
-        public static final int preferencesMailSummary=0x7f030027;
-        public static final int preferencesMailTitle=0x7f030026;
-        public static final int preferencesNotification=0x7f03000d;
-        public static final int preferencesNotificationGeocoderSummary=0x7f030014;
-        public static final int preferencesNotificationGeocoderTitle=0x7f030015;
-        public static final int preferencesNotificationLocationSummary=0x7f030012;
-        public static final int preferencesNotificationLocationTitle=0x7f030013;
-        public static final int preferencesNotificationSummary=0x7f030010;
-        public static final int preferencesNotificationTickerOnPublishSummary=0x7f03000e;
-        public static final int preferencesNotificationTickerOnPublishTitle=0x7f03000f;
-        public static final int preferencesNotificationTitle=0x7f030011;
-        public static final int preferencesQOS=0x7f03001c;
-        public static final int preferencesQOSSummary=0x7f03001d;
-        public static final int preferencesRepositorySummary=0x7f030025;
-        public static final int preferencesRepositoryTitle=0x7f030024;
-        public static final int preferencesRetain=0x7f03001a;
-        public static final int preferencesRetainSummary=0x7f03001b;
-        public static final int preferencesServerAddress=0x7f030005;
-        public static final int preferencesServerCancel=0x7f030008;
-        public static final int preferencesServerConnect=0x7f030009;
-        public static final int preferencesServerPort=0x7f030007;
-        public static final int preferencesServerTitle=0x7f030004;
-        public static final int preferencesTopic=0x7f030018;
-        public static final int preferencesTopicSettings=0x7f030017;
-        public static final int preferencesTopicSummary=0x7f030019;
-        public static final int preferencesVersionTitle=0x7f030016;
-        public static final int save=0x7f030031;
-        public static final int shareLocation=0x7f030030;
-        public static final int stateIdle=0x7f03003a;
-        public static final int stateLocatingFail=0x7f030037;
-        public static final int stateNotopic=0x7f030035;
-        public static final int statePublishTimeout=0x7f030036;
-        public static final int statePublished=0x7f030034;
-        public static final int statePublishing=0x7f030038;
-        public static final int stateWaiting=0x7f030039;
-        public static final int titleActivityPreferences=0x7f030002;
-        public static final int titleActivityStatus=0x7f030003;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int cancel=0x7f050035;
+        public static final int connectivityConnected=0x7f05002c;
+        public static final int connectivityConnecting=0x7f05002b;
+        public static final int connectivityDisconnected=0x7f05002e;
+        public static final int connectivityDisconnectedDataDisabled=0x7f050030;
+        public static final int connectivityDisconnectedUserDisconnect=0x7f05002f;
+        public static final int connectivityDisconnecting=0x7f05002d;
+        public static final int currentLocation=0x7f050032;
+        public static final int error=0x7f050036;
+        public static final int na=0x7f050031;
+        public static final int preferencesActivity=0x7f050021;
+        public static final int preferencesBackgroundUpdatesInterval=0x7f050022;
+        public static final int preferencesBackgroundUpdatesIntervalSummary=0x7f050023;
+        public static final int preferencesBackgroundUpdatesSummary=0x7f050025;
+        public static final int preferencesBackroundUpdates=0x7f050024;
+        public static final int preferencesBrokerClientId=0x7f050009;
+        public static final int preferencesInfo=0x7f050026;
+        public static final int preferencesLicensesDismiss=0x7f05000f;
+        public static final int preferencesLicensesSummary=0x7f05000e;
+        public static final int preferencesLicensesTitle=0x7f05000d;
+        public static final int preferencesMailSummary=0x7f05002a;
+        public static final int preferencesMailTitle=0x7f050029;
+        public static final int preferencesNotification=0x7f050010;
+        public static final int preferencesNotificationGeocoderSummary=0x7f050017;
+        public static final int preferencesNotificationGeocoderTitle=0x7f050018;
+        public static final int preferencesNotificationLocationSummary=0x7f050015;
+        public static final int preferencesNotificationLocationTitle=0x7f050016;
+        public static final int preferencesNotificationSummary=0x7f050013;
+        public static final int preferencesNotificationTickerOnPublishSummary=0x7f050011;
+        public static final int preferencesNotificationTickerOnPublishTitle=0x7f050012;
+        public static final int preferencesNotificationTitle=0x7f050014;
+        public static final int preferencesQOS=0x7f05001f;
+        public static final int preferencesQOSSummary=0x7f050020;
+        public static final int preferencesRepositorySummary=0x7f050028;
+        public static final int preferencesRepositoryTitle=0x7f050027;
+        public static final int preferencesRetain=0x7f05001d;
+        public static final int preferencesRetainSummary=0x7f05001e;
+        public static final int preferencesServerAddress=0x7f050008;
+        public static final int preferencesServerCancel=0x7f05000b;
+        public static final int preferencesServerConnect=0x7f05000c;
+        public static final int preferencesServerPort=0x7f05000a;
+        public static final int preferencesServerTitle=0x7f050007;
+        public static final int preferencesTopic=0x7f05001b;
+        public static final int preferencesTopicSettings=0x7f05001a;
+        public static final int preferencesTopicSummary=0x7f05001c;
+        public static final int preferencesVersionTitle=0x7f050019;
+        public static final int save=0x7f050034;
+        public static final int shareLocation=0x7f050033;
+        public static final int stateIdle=0x7f05003d;
+        public static final int stateLocatingFail=0x7f05003a;
+        public static final int stateNotopic=0x7f050038;
+        public static final int statePublishTimeout=0x7f050039;
+        public static final int statePublished=0x7f050037;
+        public static final int statePublishing=0x7f05003b;
+        public static final int stateWaiting=0x7f05003c;
+        public static final int titleActivityPreferences=0x7f050002;
+        public static final int titleActivityStatus=0x7f050003;
+        public static final int titleFriends=0x7f050005;
+        public static final int titleMap=0x7f050004;
+        public static final int titleStatus=0x7f050006;
     }
     public static final class style {
         /** 
@@ -106,21 +161,24 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f05000f;
-        public static final int AppTheme=0x7f050000;
-        public static final int AppTheme_ActionBar=0x7f050001;
-        public static final int AppTheme_ActionBar_Overflow=0x7f050002;
-        public static final int AppTheme_ActionBar_Text=0x7f050003;
-        public static final int actionBar=0x7f050004;
-        public static final int card=0x7f05000d;
-        public static final int cardButton=0x7f05000e;
-        public static final int listItem=0x7f05000a;
-        public static final int listItemSubtitle=0x7f05000c;
-        public static final int listItemTitle=0x7f05000b;
-        public static final int seperator=0x7f050005;
-        public static final int seperatorH=0x7f050006;
-        public static final int seperatorV=0x7f050007;
-        public static final int subtitle=0x7f050009;
-        public static final int title=0x7f050008;
+        public static final int AppBaseTheme=0x7f07000f;
+        public static final int AppTheme=0x7f070000;
+        public static final int AppTheme_ActionBar=0x7f070001;
+        public static final int AppTheme_ActionBar_Overflow=0x7f070002;
+        public static final int AppTheme_ActionBar_Text=0x7f070003;
+        public static final int actionBar=0x7f070004;
+        public static final int card=0x7f07000d;
+        public static final int cardButton=0x7f07000e;
+        public static final int listItem=0x7f07000a;
+        public static final int listItemSubtitle=0x7f07000c;
+        public static final int listItemTitle=0x7f07000b;
+        public static final int seperator=0x7f070005;
+        public static final int seperatorH=0x7f070006;
+        public static final int seperatorV=0x7f070007;
+        public static final int subtitle=0x7f070009;
+        public static final int title=0x7f070008;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
