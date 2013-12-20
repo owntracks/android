749c076ae9f09df9e4f29bfc0ba05c3817c08a94
Alexander Rust <mail@alr.st>
2013-12-20 16:54:48 +0100

    ui refinements

--- src/st/alr/mqttitude/support/Contact.java
+++ src/st/alr/mqttitude/support/Contact.java
@@ -9,6 +9,8 @@ import android.content.ContentUris;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.provider.ContactsContract;
 import android.util.Log;
@@ -172,6 +174,30 @@ public String toString() {
         return BitmapFactory.decodeStream(input);
     }
     
+    public Bitmap getRoundedShape(Bitmap scaleBitmapImage) {
+        // TODO Auto-generated method stub
+         int targetWidth = 50;
+         int targetHeight = 50;
+         Bitmap targetBitmap = Bitmap.createBitmap(targetWidth, 
+                               targetHeight,Bitmap.Config.ARGB_8888);
+
+         Canvas canvas = new Canvas(targetBitmap);
+         Path path = new Path();
+         path.addCircle(((float) targetWidth - 1) / 2,
+         ((float) targetHeight - 1) / 2,
+         (Math.min(((float) targetWidth), 
+                   ((float) targetHeight)) / 2),
+             Path.Direction.CCW);
+
+         canvas.clipPath(path);
+         Bitmap sourceBitmap = scaleBitmapImage;
+         canvas.drawBitmap(sourceBitmap, 
+                                   new Rect(0, 0, sourceBitmap.getWidth(),
+         sourceBitmap.getHeight()), 
+                                   new Rect(0, 0, targetWidth,
+         targetHeight), null);
+         return targetBitmap;
+        }
 
 
 
