6355374e23a70d0edf4b3d4ccbff4df180091569
Reto E. Koenig <knr1@bfh.ch>
2015-04-24 14:37:17 +0200

    go

--- src/main/java/org/owntracks/android/messages/TransitionMessage.java
+++ src/main/java/org/owntracks/android/messages/TransitionMessage.java
@@ -7,10 +7,9 @@ import org.json.JSONObject;
 import org.owntracks.android.db.Waypoint;
 import org.owntracks.android.model.GeocodableLocation;
 
-import java.sql.Date;
 import java.util.concurrent.TimeUnit;
 
-public class TransitionMessage extends Message{
+public class TransitionMessage extends Message {
     private GeocodableLocation location;
     private Waypoint waypoint;
     private String description;
@@ -20,15 +19,17 @@ public class TransitionMessage extends Message{
     private boolean supressesTicker;
 
     // For incoming messages
-    public TransitionMessage(JSONObject json) throws JSONException{
+    public TransitionMessage(JSONObject json) throws JSONException {
 
         try {
             setDescription(json.getString("desc"));
-        } catch (Exception e) { }
+        } catch (Exception e) {
+        }
 
         try {
-           setTrackerId(json.getString("tid"));
-        } catch (Exception e) { }
+            setTrackerId(json.getString("tid"));
+        } catch (Exception e) {
+        }
 
         try {
             if (json.getString("event").equals("enter"))
@@ -37,42 +38,47 @@ public class TransitionMessage extends Message{
                 setTransition(Geofence.GEOFENCE_TRANSITION_EXIT);
             else
                 setTransition(-1);
-        } catch (Exception e) { }
+        } catch (Exception e) {
+        }
     }
 
     // For outgoing messages
     public TransitionMessage(Waypoint w) {
         super();
-		this.transition = -1;
-		this.supressesTicker = false;
+        this.transition = -1;
+        this.supressesTicker = false;
         this.trackerId = null;
         this.trigger = null;
         this.waypoint = w;
-	}
+    }
 
-	public boolean getSupressTicker() {
-		return this.supressesTicker;
-	}
-	public void setSupressesTicker(boolean supressesTicker) {
-		this.supressesTicker = supressesTicker;
-	}
+    public boolean getSupressTicker() {
+        return this.supressesTicker;
+    }
+
+    public void setSupressesTicker(boolean supressesTicker) {
+        this.supressesTicker = supressesTicker;
+    }
 
     public Waypoint getWaypoint() {
         return this.waypoint;
     }
+
     public void setWaypoint(Waypoint waypoint) {
-		this.waypoint = waypoint;
-	}
+        this.waypoint = waypoint;
+    }
 
     public void setTransition(int transition) {
         this.transition = transition;
     }
+
     public int getTransition() {
         return this.transition;
     }
+
     public boolean hasTransition() {
-		return this.transition != -1;
-	}
+        return this.transition != -1;
+    }
 
 
     public boolean isForEnterTransition() {
@@ -84,15 +90,26 @@ public class TransitionMessage extends Message{
         return hasTransition() && this.transition == Geofence.GEOFENCE_TRANSITION_EXIT;
     }
 
-	public void setTrackerId(String tid) { this.trackerId = tid; }
-	public String getTrackerId() { return this.trackerId; }
+    public void setTrackerId(String tid) {
+        this.trackerId = tid;
+    }
 
-	public void setTrigger(String t) { this.trigger = t; }
-	public String getTrigger() { return this.trigger; }
+    public String getTrackerId() {
+        return this.trackerId;
+    }
+
+    public void setTrigger(String t) {
+        this.trigger = t;
+    }
+
+    public String getTrigger() {
+        return this.trigger;
+    }
 
     private void setDescription(String string) {
         this.description = string;
     }
+
     public String getDescription() {
         return description;
     }
@@ -102,28 +119,28 @@ public class TransitionMessage extends Message{
     }
 
     @Override
-	public String toString() {
-		return this.toJSONObject().toString();
-	}
+    public String toString() {
+        return this.toJSONObject().toString();
+    }
 
-	public JSONObject toJSONObject() {
+    public JSONObject toJSONObject() {
         JSONObject json = new JSONObject();
 
         try {
             json.put("_type", "transition")
-            .put("lat", this.waypoint.getLatitude())
-            .put("lon", this.waypoint.getLongitude())
-            .put("tst", (TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis())
+                    .put("lat", this.waypoint.getLatitude())
+                    .put("lon", this.waypoint.getLongitude())
+                    .put("tst", (TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis())))
                     .put("rad", Math.round(this.location.getLocation().getAccuracy() * 100) / 100.0d);
 
             if ((this.waypoint != null) && ((this.transition == Geofence.GEOFENCE_TRANSITION_EXIT) || (this.transition == Geofence.GEOFENCE_TRANSITION_ENTER))) {
                 if (this.waypoint.getShared())
                     json.put("desc", this.waypoint.getDescription());
-                    json.put("event", this.transition == Geofence.GEOFENCE_TRANSITION_ENTER ? "enter" : "leave");
-                }
+                json.put("event", this.transition == Geofence.GEOFENCE_TRANSITION_ENTER ? "enter" : "leave");
+            }
 
             if (this.trackerId != null && !this.trackerId.isEmpty())
-                 json.put("tid", this.trackerId);
+                json.put("tid", this.trackerId);
 
         } catch (JSONException e) {
 
