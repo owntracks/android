ede7b226c398da63c83cae8aa0f9a90662371238
Alexander Rust <mail@alr.st>
2015-09-16 19:45:24 +0200

    Added Snackbar view animation, Fixed #254.

--- src/main/java/org/owntracks/android/activities/ActivityBase.java
+++ src/main/java/org/owntracks/android/activities/ActivityBase.java
@@ -6,10 +6,16 @@ import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import org.owntracks.android.App;
+import org.owntracks.android.support.SnackbarFactory;
 
-public class ActivityBase extends AppCompatActivity {
+public class ActivityBase extends AppCompatActivity implements SnackbarFactory.SnackbarFactoryDelegate {
     private static final String TAG = "ActivityBase";
 
+    @Override
+    public View getSnackbarTargetView() {
+        return getWindow().getDecorView().findViewById(android.R.id.content);
+    }
 }
