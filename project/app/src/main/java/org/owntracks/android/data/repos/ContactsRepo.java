7177d14bdd81d71f51415e0ab092f06f94d69323
Alexander Rust <mail@alr.st>
2016-09-29 07:50:10 +0200

    cleanup

--- project/app/src/main/java/org/owntracks/android/data/repos/ContactsRepo.java
+++ project/app/src/main/java/org/owntracks/android/data/repos/ContactsRepo.java
@@ -1,10 +1,6 @@
 package org.owntracks.android.data.repos;
 
-import android.database.Observable;
 import android.databinding.ObservableList;
-import android.databinding.ObservableMap;
-
-import org.owntracks.android.data.model.Contact;
 import org.owntracks.android.messages.MessageCard;
 import org.owntracks.android.messages.MessageLocation;
 import org.owntracks.android.model.FusedContact;
