1c15f8b5ceee14c24553c3bcaca3e9cbf7c0221e
Alexander Rust <mail@alr.st>
2017-03-22 18:44:15 +0100

    Fixed parsing issues in newer Jackson version due to invalid defaultImpl

--- project/app/src/main/java/org/owntracks/android/messages/MessageTransition.java
+++ project/app/src/main/java/org/owntracks/android/messages/MessageTransition.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.google.android.gms.location.Geofence;
 
 import org.owntracks.android.support.IncomingMessageProcessor;
 import org.owntracks.android.support.OutgoingMessageProcessor;
-
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXTERNAL_PROPERTY, property = "_type")
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class MessageTransition extends MessageBase{
