1b53636d0581be08bdb4ed663fd1941476cb4bab
Alexander Rust <mail@alr.st>
2017-06-05 16:19:11 +0200

    Renamed MessageProcessor endpoint classes

--- project/app/src/main/java/org/owntracks/android/services/MessageProcessorEndpointHttp.java
+++ project/app/src/main/java/org/owntracks/android/services/MessageProcessorEndpointHttp.java
@@ -1,10 +1,8 @@
 package org.owntracks.android.services;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.support.annotation.NonNull;
@@ -12,8 +10,6 @@ import android.support.annotation.Nullable;
 import android.util.Base64;
 
 import com.google.android.gms.gcm.GcmNetworkManager;
-import com.google.android.gms.gcm.OneoffTask;
-import com.google.android.gms.gcm.Task;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.owntracks.android.App;
@@ -29,9 +25,8 @@ import org.owntracks.android.support.Events;
 import org.owntracks.android.support.OutgoingMessageProcessor;
 import org.owntracks.android.support.Preferences;
 import org.owntracks.android.support.SocketFactory;
-import org.owntracks.android.support.interfaces.StatelessMessageEndpoint;
 import org.owntracks.android.support.Parser;
-import org.owntracks.android.services.ServiceMessage.EndpointState;
+import org.owntracks.android.services.MessageProcessor.EndpointState;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -42,9 +37,7 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 import javax.net.ssl.X509TrustManager;
 
@@ -55,7 +48,7 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import timber.log.Timber;
 
-public class ServiceMessageHttp implements OutgoingMessageProcessor {
+public class MessageProcessorEndpointHttp implements OutgoingMessageProcessor {
     // Headers according to https://github.com/owntracks/recorder#http-mode
     private static final String HEADER_USERNAME = "X-Limit-U";
     private static final String HEADER_DEVICE = "X-Limit-D";
@@ -73,27 +66,19 @@ public class ServiceMessageHttp implements OutgoingMessageProcessor {
     private String endpointUserInfo;
 
     private static OkHttpClient mHttpClient;
-    public static final MediaType JSON  = MediaType.parse("application/json; charset=utf-8");
-    private ThreadPoolExecutor mOutgoingMessageProcessorExecutor;
-    private PowerManager powerManager;
-    private ConnectivityManager connectivityManager;
-    private ServiceMessage service;
-    private ServiceProxy context;
+    private static final MediaType JSON  = MediaType.parse("application/json; charset=utf-8");
+    private MessageProcessor service;
 
-    public void onCreate(ServiceProxy c) {
-        Timber.v("loaded HTTP endoint");
-
-    }
-
-    private static ServiceMessageHttp instance;
-    public static ServiceMessageHttp getInstance() {
+    private static MessageProcessorEndpointHttp instance;
+    public static MessageProcessorEndpointHttp getInstance() {
         if(instance == null) {
-            instance = new ServiceMessageHttp();
+            instance = new MessageProcessorEndpointHttp();
         }
         return instance;
     }
 
-    public ServiceMessageHttp() {
+
+    public MessageProcessorEndpointHttp() {
         Preferences.registerOnPreferenceChangedListener(new Preferences.OnPreferenceChangedListener() {
             @Override
             public void onAttachAfterModeChanged() {
@@ -119,6 +104,10 @@ public class ServiceMessageHttp implements OutgoingMessageProcessor {
         loadEndpointUrl();
         loadHTTPClient();
     }
+    @Override
+    public void onCreateFromProcessor() {
+
+    }
 
     private void loadHTTPClient() {
         String tlsCaCrt = Preferences.getTlsCaCrtName();
@@ -127,7 +116,7 @@ public class ServiceMessageHttp implements OutgoingMessageProcessor {
 
         if (tlsCaCrt.length() > 0) {
             try {
-                socketFactoryOptions.withCaInputStream(context.openFileInput(tlsCaCrt));
+                socketFactoryOptions.withCaInputStream(App.getContext().openFileInput(tlsCaCrt));
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
@@ -135,7 +124,7 @@ public class ServiceMessageHttp implements OutgoingMessageProcessor {
 
         if (tlsClientCrt.length() > 0)	{
             try {
-                socketFactoryOptions.withClientP12InputStream(context.openFileInput(tlsClientCrt)).withClientP12Password(Preferences.getTlsClientCrtPassword());
+                socketFactoryOptions.withClientP12InputStream(App.getContext().openFileInput(tlsClientCrt)).withClientP12Password(Preferences.getTlsClientCrtPassword());
             } catch (FileNotFoundException e1) {
                 e1.printStackTrace();
             }
@@ -221,17 +210,21 @@ public class ServiceMessageHttp implements OutgoingMessageProcessor {
                     ServiceProxy.getServiceMessage().onEndpointStateChanged(EndpointState.IDLE, "Response "+r.code() + ", " + result.length);
 
                     for (MessageBase aResult : result) {
-                        onMessageReceived(aResult);
+                        getMessageProcessor().onMessageReceived(aResult);
                     }
 
                     //Non JSON return value
                 } catch (IOException e) {
                     ServiceProxy.getServiceMessage().onEndpointStateChanged(EndpointState.ERROR, "HTTP " +r.code() + ", JsonParseException");
                     Timber.e("error:JsonParseException responseCode:%s", r.code());
-                    e.printStackTrace();                } catch (Parser.EncryptionException e) {
-
+                    e.printStackTrace();
+                    getMessageProcessor().onMessageDeliveryFailed(messageId);
+                    return false;
+                } catch (Parser.EncryptionException e) {
                     ServiceProxy.getServiceMessage().onEndpointStateChanged(EndpointState.ERROR, "Response: "+r.code() + ", EncryptionException");
                     Timber.e("error:EncryptionException");
+                    getMessageProcessor().onMessageDeliveryFailed(messageId);
+                    return false;
                 }
                 return true;
             } else {
@@ -245,110 +238,18 @@ public class ServiceMessageHttp implements OutgoingMessageProcessor {
         }
     }
 
-    @Deprecated
-    public static int postMessage(final String body, @Nullable final String url, @Nullable final String userInfo, final Context c, final Long messageId) {
-        Timber.v("url:%s, userInfo:%s, messageId:%s", url, userInfo,  messageId);
-
-        if(url == null) {
-            Timber.e("url not configured. messageId:%s", messageId);
-            return GcmNetworkManager.RESULT_FAILURE;
-        }
-
-        if(c instanceof ServiceMessageHttpGcm && mHttpClient == null) {
-            Timber.e("sevice not available. Binding and rescheduling GCM task");
-            ServiceProxy.bind(App.getContext());
-            return GcmNetworkManager.RESULT_RESCHEDULE;
-        }
-
-
-
-        Request.Builder request = new Request.Builder().url(url).method("POST", RequestBody.create(JSON, body));
-
-         if(userInfo != null) {
-            request.header(HEADER_AUTHORIZATION, "Basic " + android.util.Base64.encodeToString(userInfo.getBytes(), Base64.NO_WRAP));
-        } else if(Preferences.getAuth()) {
-             request.header(HEADER_AUTHORIZATION, "Basic " + android.util.Base64.encodeToString((Preferences.getUsername()+":"+Preferences.getPassword()).getBytes(), Base64.NO_WRAP));
-
-         }
-        
-        if(headerUsername != null) {
-            request.header(HEADER_USERNAME, headerUsername);
-        }
-        if(headerDevice != null) {
-            request.header(HEADER_DEVICE, headerDevice);
-        }
-
-        try {
-             Response r = mHttpClient.newCall(request.build()).execute();
-
-             if((r != null) && (r.isSuccessful())) {
-                 Timber.v("got HTTP response");
-
-                 try {
-                     //Timber.v("code: %s, streaming response to parser", r.body().string() );
-
-                     MessageBase[] result = Parser.fromJson(r.body().byteStream());
-                     ServiceProxy.getServiceMessage().onEndpointStateChanged(EndpointState.IDLE, "Response "+r.code() + ", " + result.length);
-
-                     for (MessageBase aResult : result) {
-                        onMessageReceived(aResult);
-                    }
-
-                //Non JSON return value
-                } catch (IOException e) {
-                     ServiceProxy.getServiceMessage().onEndpointStateChanged(EndpointState.ERROR, "HTTP " +r.code() + ", JsonParseException");
-                    Timber.e("error:JsonParseException responseCode:%s", r.code());
-e.printStackTrace();                } catch (Parser.EncryptionException e) {
-
-                     ServiceProxy.getServiceMessage().onEndpointStateChanged(EndpointState.ERROR, "Response: "+r.code() + ", EncryptionException");
-                     Timber.e("error:EncryptionException");
-                 }
-                 return onMessageDelivered(c, messageId);
-            } else {
-                return onMessageDeliveryFailed(messageId);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            ServiceProxy.getServiceMessage().onEndpointStateChanged(EndpointState.ERROR, e);
-            return onMessageDeliveryFailed(messageId);
-        }
+    private static MessageProcessor getMessageProcessor() {
+        return App.getMessageProcessor();
     }
 
-
-    private static int onMessageDelivered(@NonNull Context c, @Nullable Long messageId) {
-        if(messageId == null || messageId == 0) {
-            Timber.e("messageId:null");
-            return GcmNetworkManager.RESULT_SUCCESS;
-        }
-
-        ServiceProxy.getServiceMessage().onMessageDelivered(messageId);
-        return GcmNetworkManager.RESULT_SUCCESS;
-    }
-
-    private static int onMessageDeliveryFailed(Long messageId) {
-        if(messageId == null || messageId == 0) {
-            Timber.e("messageId:null");
-        }
-
-        return GcmNetworkManager.RESULT_FAILURE;
-    }
-
-    private static void onMessageReceived(@NonNull MessageBase message) {
-        ServiceProxy.getServiceMessage().onMessageReceived(message);
-    }
-
-
-
-    Bundle httpMessageToBundle(MessageBase m) throws IOException, Parser.EncryptionException {
+    private Bundle httpMessageToBundle(MessageBase m) throws IOException, Parser.EncryptionException {
         Bundle b = new Bundle();
         b.putString(HTTP_BUNDLE_KEY_MESSAGE_PAYLOAD, Parser.toJson(m));
         b.putString(HTTP_BUNDLE_KEY_USERINFO, getInstance().endpointUserInfo);
         b.putString(HTTP_BUNDLE_KEY_URL, getInstance().endpointUrl);
-        b.putLong(Dispatcher.BUNDLE_KEY_MESSAGE_ID, m.getMessageId());
+        b.putLong(Scheduler.BUNDLE_KEY_MESSAGE_ID, m.getMessageId());
         return b;
     }
-
 
     @Override
     public void processOutgoingMessage(MessageBase message) {
@@ -391,20 +292,23 @@ e.printStackTrace();                } catch (Parser.EncryptionException e) {
     }
 
     @Override
-    public void onAssociate(ServiceMessage service) {
-        this.service = service;
+    public void onDestroy() {
+
     }
 
     @Override
-    public void onDestroy() {
+    public void onEnterForeground() {
 
     }
 
     private void scheduleMessage(MessageBase m) {
         try {
             Bundle b = httpMessageToBundle(m);
-            b.putString(Dispatcher.BUNDLE_KEY_ACTION, Dispatcher.TASK_SEND_MESSAGE_MQTT);
-            App.getDispatcher().scheduleMessage(b);
+            b.putString(Scheduler.BUNDLE_KEY_ACTION, Scheduler.TASK_SEND_MESSAGE_MQTT);
+            if(App.isInForeground())
+                sendMessage(b);
+            else
+                App.getDispatcher().scheduleMessage(b);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (Parser.EncryptionException e) {
