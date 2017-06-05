445a05f6ab84cd6f78dbf85e4a18dfdce39e54da
Alexander Rust <mail@alr.st>
2017-06-05 16:18:36 +0200

    Renamed Dispatcher to Scheduler

--- project/app/src/main/java/org/owntracks/android/services/Scheduler.java
+++ project/app/src/main/java/org/owntracks/android/services/Scheduler.java
@@ -1,36 +1,35 @@
 package org.owntracks.android.services;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
 
 import com.google.android.gms.gcm.GcmNetworkManager;
 import com.google.android.gms.gcm.GcmTaskService;
 import com.google.android.gms.gcm.OneoffTask;
+import com.google.android.gms.gcm.PeriodicTask;
 import com.google.android.gms.gcm.Task;
 import com.google.android.gms.gcm.TaskParams;
 
 import org.owntracks.android.App;
-import org.owntracks.android.messages.MessageBase;
-import org.owntracks.android.support.Parser;
-import org.owntracks.android.support.Preferences;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import timber.log.Timber;
 
-public class Dispatcher extends GcmTaskService {
+public class Scheduler extends GcmTaskService {
     public static final String BUNDLE_KEY_ACTION = "DISPATCHER_ACTION";
     public static final String BUNDLE_KEY_MESSAGE_ID = "MESSAGE_ID";
 
     public static final String TASK_SEND_MESSAGE_HTTP = "SEND_MESSAGE_HTTP";
     public static final String TASK_SEND_MESSAGE_MQTT = "SEND_MESSAGE_MQTT";
+    private static final String TASK_SEND_MQTT_PING = "TASK_SEND_MQTT_PING" ;
+    private static final String TASK_SEND_LOCATION_PING = "TASK_SEND_LOCATION_PING" ;
 
     @Override
     public int onRunTask(TaskParams taskParams) {
         Bundle extras = taskParams.getExtras();
         String action = extras.getString(BUNDLE_KEY_ACTION);
+
         if(action == null) {
             Timber.e("BUNDLE_KEY_ACTION is not set");
             return GcmNetworkManager.RESULT_FAILURE;
@@ -40,9 +39,11 @@ public class Dispatcher extends GcmTaskService {
 
         switch (action) {
             case TASK_SEND_MESSAGE_HTTP:
-                return ServiceMessageHttp.getInstance().sendMessage(extras) ? GcmNetworkManager.RESULT_SUCCESS : GcmNetworkManager.RESULT_FAILURE;
+                return MessageProcessorEndpointHttp.getInstance().sendMessage(extras) ? GcmNetworkManager.RESULT_SUCCESS : GcmNetworkManager.RESULT_FAILURE;
             case TASK_SEND_MESSAGE_MQTT:
-                return ServiceEndpointMqtt.getInstance().sendMessage(extras) ? GcmNetworkManager.RESULT_SUCCESS : GcmNetworkManager.RESULT_FAILURE;
+                return MessageProcessorEndpointMqtt.getInstance().sendMessage(extras) ? GcmNetworkManager.RESULT_SUCCESS : GcmNetworkManager.RESULT_FAILURE;
+            case TASK_SEND_MQTT_PING:
+                return MessageProcessorEndpointMqtt.getInstance().sendPing() ? GcmNetworkManager.RESULT_SUCCESS : GcmNetworkManager.RESULT_FAILURE;
             default:
                 return GcmNetworkManager.RESULT_FAILURE;
         }
@@ -59,10 +60,8 @@ public class Dispatcher extends GcmTaskService {
             return;
         }
 
-
-
         Task task = new OneoffTask.Builder()
-                .setService(Dispatcher.class)
+                .setService(Scheduler.class)
                 .setExecutionWindow(0L, App.isInForeground() ? 1L: 60L)
                 .setTag(Long.toString(b.getLong(BUNDLE_KEY_MESSAGE_ID)))
                 .setUpdateCurrent(false)
@@ -71,8 +70,33 @@ public class Dispatcher extends GcmTaskService {
                 .setExtras(b)
                 .build();
 
-        Timber.v("scheduling task %s /", task.getTag());
+        Timber.v("scheduling task %s, %s", b.get(BUNDLE_KEY_ACTION), task.getTag());
 
         GcmNetworkManager.getInstance(App.getContext()).schedule(task);
+    }
+
+    public void scheduleMqttPing(long keepAliveMs) {
+        PeriodicTask task = new PeriodicTask.Builder()
+                .setService(Scheduler.class)
+                .setTag(TASK_SEND_MQTT_PING)
+                .setExtras(getBundleForAction(TASK_SEND_MQTT_PING))
+                .setPeriod(TimeUnit.MILLISECONDS.toSeconds(keepAliveMs))
+                .setUpdateCurrent(true)
+                .setRequiredNetwork(Task.NETWORK_STATE_CONNECTED)
+                .setFlex(TimeUnit.MILLISECONDS.toSeconds(keepAliveMs))
+                .build();
+        Timber.v("scheduling task TASK_SEND_MQTT_PING");
+        GcmNetworkManager.getInstance(App.getContext()).schedule(task);
+    }
+
+    @NonNull
+    public static Bundle getBundleForAction(String action) {
+        Bundle b  = new Bundle();
+        b.putString(BUNDLE_KEY_ACTION, action);
+        return b;
+    }
+
+    public void scheduleLocationPing() {
+        Bundle b = getBundleForAction(TASK_SEND_LOCATION_PING);
     }
 }
