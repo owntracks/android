8a7fd31b0645caa5133eaa9ab0856a5135277c60
Alexander Rust <mail@alr.st>
2017-06-10 12:54:20 +0200

    Moving notification management out of the background service

--- project/app/src/main/java/org/owntracks/android/injection/modules/AppModule.java
+++ project/app/src/main/java/org/owntracks/android/injection/modules/AppModule.java
@@ -3,19 +3,22 @@ package org.owntracks.android.injection.modules;
 import android.app.Application;
 import android.content.Context;
 import android.content.res.Resources;
+import android.os.Build;
 
 import org.greenrobot.eventbus.EventBus;
 import org.owntracks.android.data.repos.ContactsRepo;
 import org.owntracks.android.injection.qualifier.AppContext;
-import org.owntracks.android.injection.scopes.PerActivity;
 import org.owntracks.android.injection.scopes.PerApplication;
 import org.owntracks.android.services.MessageProcessor;
 import org.owntracks.android.services.Scheduler;
 import org.owntracks.android.support.ContactImageProvider;
 import org.owntracks.android.support.EncryptionProvider;
 import org.owntracks.android.support.GeocodingProvider;
+import org.owntracks.android.support.NotificationProvider;
 import org.owntracks.android.support.Parser;
 import org.owntracks.android.support.Preferences;
+
+import java.util.Locale;
 
 import dagger.Module;
 import dagger.Provides;
@@ -74,6 +77,14 @@ public class AppModule {
         return new MessageProcessor(eventBus, repo);
     }
 
+    @SuppressWarnings("deprecation")
+    @Provides
+    @PerApplication
+    static Locale provideLocale(@AppContext Context context) {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ? context.getResources().getConfiguration().getLocales().get(0) : context.getResources().getConfiguration().locale;
+    }
+
+
     @Provides
     @PerApplication
     static Parser provideParser(EncryptionProvider provider) {
@@ -98,4 +109,9 @@ public class AppModule {
     @Provides
     @PerApplication
     static Preferences providePreferences(@AppContext Context context) { return new Preferences(context); }
+
+    @Provides
+    @PerApplication
+    static NotificationProvider provideNotificationProvider(@AppContext Context context, Resources resources, Locale locale) { return new NotificationProvider(context, resources, locale); }
+
 }
