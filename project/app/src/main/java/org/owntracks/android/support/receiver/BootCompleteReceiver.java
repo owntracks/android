6d6891a038414592ce7489730fab901a476b1911
Alexander Rust <mail@alr.st>
2017-06-22 19:37:09 +0200

    preparing for Android 8 service startup

--- project/app/src/main/java/org/owntracks/android/support/receiver/BootCompleteReceiver.java
+++ project/app/src/main/java/org/owntracks/android/support/receiver/BootCompleteReceiver.java
@@ -1,5 +1,6 @@
 package org.owntracks.android.support.receiver;
 
+import org.owntracks.android.App;
 import org.owntracks.android.support.Preferences;
 
 import android.content.BroadcastReceiver;
@@ -11,8 +12,7 @@ public class BootCompleteReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction()) && Preferences.getAutostartOnBoot()){
-			//TODO: reimplement
-			// context.startService(new Intent(context, ServiceProxy.class));
+			App.startBackgroundServiceCompat(context, null);
 		}
 	}
 }
