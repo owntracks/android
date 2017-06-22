30ebfde85dab8a57de14f86d01a3dbdc499cac9f
Alexander Rust <mail@alr.st>
2017-06-22 19:39:08 +0200

    Added handling of missing Play Services

--- project/app/src/main/java/org/owntracks/android/ui/welcome/play/PlayFragmentMvvm.java
+++ project/app/src/main/java/org/owntracks/android/ui/welcome/play/PlayFragmentMvvm.java
@@ -1,13 +1,22 @@
 package org.owntracks.android.ui.welcome.play;
 
+import android.databinding.Bindable;
+
 import org.owntracks.android.ui.base.view.MvvmView;
 import org.owntracks.android.ui.welcome.WelcomeFragmentMvvm;
 
 
 public interface PlayFragmentMvvm {
     interface View extends WelcomeFragmentMvvm.View {
-
+        void checkAvailability();
+        void requestFix();
     }
+
     interface ViewModel<V extends MvvmView> extends WelcomeFragmentMvvm.ViewModel<V> {
+        void onFixClicked();
+
+        @Bindable boolean isFixAvailable();
+        @Bindable void setFixAvailable(boolean enabled);
+
     }
 }
