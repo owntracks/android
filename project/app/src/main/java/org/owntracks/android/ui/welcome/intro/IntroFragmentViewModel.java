68ecd6763fb5523341bf318910f3c4d21b2d93ed
Alexander Rust <mail@alr.st>
2017-06-22 07:24:23 +0200

    Added codestyle

--- project/app/src/main/java/org/owntracks/android/ui/welcome/intro/IntroFragmentViewModel.java
+++ project/app/src/main/java/org/owntracks/android/ui/welcome/intro/IntroFragmentViewModel.java
@@ -5,14 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import org.owntracks.android.App;
-import org.owntracks.android.BR;
-import org.owntracks.android.R;
 import org.owntracks.android.injection.scopes.PerFragment;
-import org.owntracks.android.support.Preferences;
 import org.owntracks.android.ui.base.viewmodel.BaseViewModel;
-import org.owntracks.android.ui.welcome.WelcomeFragmentMvvm;
-import org.owntracks.android.ui.welcome.mode.ModeFragmentMvvm;
 
 import javax.inject.Inject;
 
