865555405ddc93a3283bd0fe836d9d4f2080e3e3
Alexander Rust <mail@alr.st>
2018-11-20 21:42:12 +0100

    Added logging to BootCompleteReceiver

--- project/app/src/main/java/org/owntracks/android/support/receiver/BootCompleteReceiver.java
+++ project/app/src/main/java/org/owntracks/android/support/receiver/BootCompleteReceiver.java
@@ -23,9 +23,12 @@ public class BootCompleteReceiver extends DaggerBroadcastReceiver {
 		super.onReceive(context,intent);
 
 		if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction()) && preferences.getAutostartOnBoot()){
+			Timber.v("android.intent.action.BOOT_COMPLETED received");
 			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+				Timber.v("running startForegroundService");
 				context.startForegroundService((new Intent(context, BackgroundService.class)));
 			} else {
+				Timber.v("running legacy startService");
 				context.startService((new Intent(context, BackgroundService.class)));
 			}
 		}
