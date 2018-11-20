a5e4750b54e67775ba10722361d0645a186306a7
Alexander Rust <mail@alr.st>
2018-11-20 21:42:27 +0100

    Added Tasker broadcast receiver

---
+++ project/app/src/main/java/org/owntracks/android/support/receiver/TaskerReceiver.java
@@ -0,0 +1,17 @@
+package org.owntracks.android.support.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import timber.log.Timber;
+
+public final class TaskerReceiver extends BroadcastReceiver
+{
+
+
+    @Override
+    public void onReceive(final Context context, final Intent intent)
+    {
+        Timber.v("Receiver", "Fired");
+    }
+}
