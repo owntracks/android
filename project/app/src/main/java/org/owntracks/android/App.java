6cc93ab3330bfa33fd0192767d4784f3906955c7
Alexander Rust <mail@alr.st>
2018-12-08 07:10:48 +0100

    Make Roboelectric tests pass by using a custom WorkManagerInitializer

--- project/app/src/main/java/org/owntracks/android/App.java
+++ project/app/src/main/java/org/owntracks/android/App.java
@@ -50,6 +50,7 @@ public class App extends DaggerApplication  {
     @Override
     public void onCreate() {
         sInstance = this;
+        WorkManager.initialize(this, new Configuration.Builder().build());
 
         super.onCreate();
 
