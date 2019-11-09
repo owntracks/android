a4ee42173f9015819079b3b641021783c7b23819
Andrew Rowson <github@growse.com>
2019-11-09 21:06:40 +0000

    Fixed MQTT waypoints topic suffix to "waypoints" from "waypoint". Fix for #762

--- project/app/src/main/java/org/owntracks/android/support/Preferences.java
+++ project/app/src/main/java/org/owntracks/android/support/Preferences.java
@@ -487,7 +487,7 @@ public class Preferences {
     }
 
     public String getPubTopicWaypointsPart() {
-        return "/waypoint";
+        return "/waypoints";
     }
 
     public String getPubTopicEvents() {
