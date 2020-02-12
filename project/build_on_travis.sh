174c62e69adf1d796b71501911101ec1a37b0c3d
Andrew Rowson <github@growse.com>
2020-02-12 10:12:07 +0000

    Testing out github actions for building and publishing
    * Created initial gh actions workflow
    * removed travis config

--- project/build_on_travis.sh
+++
@@ -1,6 +0,0 @@
-
-set -ev
-
-./gradlew assembleRelease
-jarsigner -verbose -sigalg SHA1withRSA -storepass $keystorepass -keypass $keystorepass -digestalg SHA1 -keystore ../keystore.jks ./app/build/outputs/apk/release/app-release-unsigned.apk owntracks
-/usr/local/android-sdk/build-tools/29.0.2/zipalign -v 4 ./app/build/outputs/apk/release/app-release-unsigned.apk ./app/build/outputs/apk/release/app-release.apk
