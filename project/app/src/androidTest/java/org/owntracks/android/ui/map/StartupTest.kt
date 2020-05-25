68689fbd12586191633b657f9d8efb58f932b9fd
Andrew Rowson <github@growse.com>
2020-05-25 18:43:24 +0100

    Let's replace robolectric tests with an espresso test and see what happens

---
+++ project/app/src/androidTest/java/org/owntracks/android/ui/map/StartupTest.kt
@@ -0,0 +1,64 @@
+package org.owntracks.android.ui.map
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.GrantPermissionRule
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.owntracks.android.R
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class StartupTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityScenarioRule(MapActivity::class.java)
+
+    @Rule
+    @JvmField
+    var mGrantPermissionRule =
+            GrantPermissionRule.grant(
+                    "android.permission.ACCESS_FINE_LOCATION")
+
+    @Test
+    fun startupTest() {
+        val textView = onView(
+                allOf(withId(R.id.screen_heading), withText("Welcome"),
+                        childAtPosition(
+                                allOf(withId(R.id.welcome_fragment),
+                                        withParent(withId(R.id.viewPager))),
+                                1),
+                        isDisplayed()))
+        textView.check(matches(withText("Welcome")))
+    }
+
+    private fun childAtPosition(
+            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
