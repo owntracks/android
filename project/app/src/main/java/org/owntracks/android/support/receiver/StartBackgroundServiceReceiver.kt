0e966cda8a09e32b9fca94751e7a8b631aa6c42c
Andrew Rowson <github@growse.com>
2020-05-26 15:25:10 +0100

    Run connectedtest on api 21 and 29. Filter startup receiver intent correctly

---
+++ project/app/src/main/java/org/owntracks/android/support/receiver/StartBackgroundServiceReceiver.kt
@@ -0,0 +1,30 @@
+package org.owntracks.android.support.receiver
+
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import dagger.android.DaggerBroadcastReceiver
+import org.owntracks.android.services.BackgroundService
+import org.owntracks.android.support.Preferences
+import timber.log.Timber
+import javax.inject.Inject
+
+class StartBackgroundServiceReceiver : DaggerBroadcastReceiver() {
+    @JvmField
+    @Inject
+    var preferences: Preferences? = null
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        if ("android.intent.action.MY_PACKAGE_REPLACED" == intent.action ||
+                "android.intent.action.BOOT_COMPLETED" == intent.action && preferences!!.autostartOnBoot) {
+            Timber.v("android.intent.action.BOOT_COMPLETED received")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                Timber.v("running startForegroundService")
+                context.startForegroundService(Intent(context, BackgroundService::class.java))
+            } else {
+                Timber.v("running legacy startService")
+                context.startService(Intent(context, BackgroundService::class.java))
+            }
+        }
+    }
+}
