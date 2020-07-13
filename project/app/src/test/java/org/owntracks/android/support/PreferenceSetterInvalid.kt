04b4152bde081fab62f1335aa8e1dfd9b641827a
Andrew Rowson <github@growse.com>
2020-07-13 17:15:56 +0100

    Added some tests for invalid data importing to preferences

---
+++ project/app/src/test/java/org/owntracks/android/support/PreferenceSetterInvalid.kt
@@ -0,0 +1,55 @@
+package org.owntracks.android.support
+
+import android.content.Context
+import android.content.res.Resources
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.owntracks.android.messages.MessageConfiguration
+import org.owntracks.android.support.preferences.PreferencesStore
+
+class PreferenceSetterInvalid {
+    private lateinit var mockResources: Resources
+    private lateinit var mockContext: Context
+    private lateinit var preferencesStore: PreferencesStore
+
+    @Before
+    fun createMocks() {
+        mockResources = PreferencesGettersAndSetters.getMockResources()
+        mockContext = mock {
+            on { resources } doReturn mockResources
+            on { packageName } doReturn javaClass.canonicalName
+        }
+        preferencesStore = InMemoryPreferencesStore()
+    }
+
+    @Test
+    fun `when given an import value of null, the config value should be cleared`() {
+        val preferences = Preferences(mockContext, null, preferencesStore)
+        val messageConfiguration = MessageConfiguration()
+        messageConfiguration.set("host", null)
+        preferences.host = "testHost"
+        preferences.importFromMessage(messageConfiguration)
+        Assert.assertEquals("", preferences.host)
+    }
+
+    @Test
+    fun `when given an invalid preference key, it should be ignored`() {
+        val preferences = Preferences(mockContext, null, preferencesStore)
+        val messageConfiguration = MessageConfiguration()
+        messageConfiguration.set("Invalid", "invalid")
+        preferences.importFromMessage(messageConfiguration)
+        Assert.assertFalse(preferences.exportToMessage().keys.contains("Invalid"))
+    }
+
+    @Test
+    fun `when given an import value of the wrong type, it should be ignored`() {
+        val preferences = Preferences(mockContext, null, preferencesStore)
+        val messageConfiguration = MessageConfiguration()
+        messageConfiguration.set("host", 4)
+        preferences.importFromMessage(messageConfiguration)
+        Assert.assertEquals("", preferences.host)
+    }
+}
