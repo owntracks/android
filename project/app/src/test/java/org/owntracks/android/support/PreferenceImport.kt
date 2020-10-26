c63a61dc02248189c197b0895bccbc67b73c4d70
Andrew Rowson <github@growse.com>
2020-10-26 10:32:51 +0000

    Fixed bug where only the first config key gets imported

--- project/app/src/test/java/org/owntracks/android/support/PreferenceImport.kt
+++ project/app/src/test/java/org/owntracks/android/support/PreferenceImport.kt
@@ -4,13 +4,14 @@ import android.content.Context
 import android.content.res.Resources
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
-import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Before
 import org.junit.Test
 import org.owntracks.android.model.messages.MessageConfiguration
 import org.owntracks.android.support.preferences.PreferencesStore
 
-class PreferenceSetterInvalid {
+class PreferenceImport {
     private lateinit var mockResources: Resources
     private lateinit var mockContext: Context
     private lateinit var preferencesStore: PreferencesStore
@@ -26,30 +27,43 @@ class PreferenceSetterInvalid {
     }
 
     @Test
-    fun `when given an import value of null, the config value should be cleared`() {
+    fun `given a configuration message, when importing to preferences, all the keys in the config should be added`() {
+        val preferences = Preferences(mockContext, null, preferencesStore)
+        val messageConfiguration = MessageConfiguration()
+        messageConfiguration["autostartOnBoot"] = true
+        messageConfiguration["host"] = "testhost"
+        messageConfiguration["port"] = 1234
+        preferences.importFromMessage(messageConfiguration)
+        assertEquals(true, preferences.autostartOnBoot)
+        assertEquals("testhost", preferences.host)
+        assertEquals(1234, preferences.port)
+    }
+
+    @Test
+    fun `given a configuration message with an entry value of null, when importing to preferences, the config value should be cleared`() {
         val preferences = Preferences(mockContext, null, preferencesStore)
         val messageConfiguration = MessageConfiguration()
         messageConfiguration["host"] = null
         preferences.host = "testHost"
         preferences.importFromMessage(messageConfiguration)
-        Assert.assertEquals("", preferences.host)
+        assertEquals("", preferences.host)
     }
 
     @Test
-    fun `when given an invalid preference key, it should be ignored`() {
+    fun `given a configuration message with an invalid key, when importing to preferences, it should be ignored`() {
         val preferences = Preferences(mockContext, null, preferencesStore)
         val messageConfiguration = MessageConfiguration()
         messageConfiguration["Invalid"] = "invalid"
         preferences.importFromMessage(messageConfiguration)
-        Assert.assertFalse(preferences.exportToMessage().keys.contains("Invalid"))
+        assertFalse(preferences.exportToMessage().keys.contains("Invalid"))
     }
 
     @Test
-    fun `when given an import value of the wrong type, it should be ignored`() {
+    fun `given a configuration message with a value of the wrong type, when importing to preferences, it should be ignored`() {
         val preferences = Preferences(mockContext, null, preferencesStore)
         val messageConfiguration = MessageConfiguration()
         messageConfiguration["host"] = 4
         preferences.importFromMessage(messageConfiguration)
-        Assert.assertEquals("", preferences.host)
+        assertEquals("", preferences.host)
     }
 }
