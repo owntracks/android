15ea7b6451644e4887cfd64717ebaba2fa0dc572
Andrew Rowson <github@growse.com>
2020-11-05 11:06:30 +0000

    App refactor

---
+++ project/app/src/main/java/org/owntracks/android/App.kt
@@ -0,0 +1,86 @@
+package org.owntracks.android
+
+import android.content.Intent
+import android.os.StrictMode
+import android.os.StrictMode.VmPolicy
+import androidx.work.Configuration
+import androidx.work.WorkManager
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.owntracks.android.injection.components.AppComponentProvider
+import org.owntracks.android.injection.components.DaggerAppComponent
+import org.owntracks.android.services.MessageProcessor
+import org.owntracks.android.support.Events.RestartApp
+import org.owntracks.android.support.Preferences
+import org.owntracks.android.support.RunThingsOnOtherThreads
+import org.owntracks.android.support.TimberDebugLogTree
+import org.owntracks.android.ui.map.MapActivity
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+import javax.inject.Inject
+
+class App : DaggerApplication() {
+    @JvmField
+    @Inject
+    var preferences: Preferences? = null
+
+    @JvmField
+    @Inject
+    var runThingsOnOtherThreads: RunThingsOnOtherThreads? = null
+
+    @JvmField
+    @Inject
+    var messageProcessor: MessageProcessor? = null
+
+    @JvmField
+    @Inject
+    var eventBus: EventBus? = null
+    override fun onCreate() {
+        WorkManager.initialize(this, Configuration.Builder().build())
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(TimberDebugLogTree())
+            Timber.e("StrictMode enabled in DEBUG build")
+            StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .penaltyFlashScreen()
+                    .penaltyDialog()
+                    .build())
+            StrictMode.setVmPolicy(VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .detectFileUriExposure()
+                    .penaltyLog()
+                    .build())
+        } else {
+            Timber.plant(DebugTree())
+        }
+        for (t in Timber.forest()) {
+            Timber.v("Planted trees :%s", t)
+        }
+        preferences!!.checkFirstStart()
+
+        // Running this on a background thread will deadlock FirebaseJobDispatcher.
+        // Initialize will call Scheduler to connect off the main thread anyway.
+        runThingsOnOtherThreads!!.postOnMainHandlerDelayed(Runnable { messageProcessor!!.initialize() }, 510)
+        eventBus!!.register(this)
+    }
+
+    @Subscribe
+    fun onEvent(e: RestartApp?) {
+        val intent = Intent(this.applicationContext, MapActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        this.applicationContext.startActivity(intent)
+        Runtime.getRuntime().exit(0)
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        val appComponent = DaggerAppComponent.builder().app(this).build()
+        appComponent.inject(this)
+        AppComponentProvider.setAppComponent(appComponent)
+        return appComponent
+    }
+}
