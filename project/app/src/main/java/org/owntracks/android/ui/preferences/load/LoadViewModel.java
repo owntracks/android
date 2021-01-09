81a985f78bed99dae87c9ad4732bd6912dd8e7d7
Andrew Rowson <github@growse.com>
2021-01-09 15:43:18 +0000

    Refactor LoadActivity MV to move import logic there

--- project/app/src/main/java/org/owntracks/android/ui/preferences/load/LoadViewModel.java
+++ project/app/src/main/java/org/owntracks/android/ui/preferences/load/LoadViewModel.java
@@ -1,12 +1,18 @@
 package org.owntracks.android.ui.preferences.load;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.MutableLiveData;
 
+import com.fasterxml.jackson.core.JsonParseException;
+
+import org.owntracks.android.R;
 import org.owntracks.android.data.repos.WaypointsRepo;
 import org.owntracks.android.injection.qualifier.AppContext;
 import org.owntracks.android.injection.scopes.PerActivity;
@@ -16,10 +22,23 @@ import org.owntracks.android.support.Parser;
 import org.owntracks.android.support.Preferences;
 import org.owntracks.android.ui.base.viewmodel.BaseViewModel;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
 
 import javax.inject.Inject;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 import timber.log.Timber;
 
 
@@ -30,8 +49,9 @@ public class LoadViewModel extends BaseViewModel<LoadMvvm.View> implements LoadM
     private final WaypointsRepo waypointsRepo;
 
     private MessageConfiguration configuration;
-    private MutableLiveData<Boolean> hasConfigurationLive = new MutableLiveData<>();
-    private MutableLiveData<String> formattedEffectiveConfiguration = new MutableLiveData<>();
+    private final MutableLiveData<String> formattedEffectiveConfiguration = new MutableLiveData<>();
+    private final MutableLiveData<Throwable> importFailure = new MutableLiveData<>();
+    private Context context;
 
 
     @Inject
@@ -39,17 +59,17 @@ public class LoadViewModel extends BaseViewModel<LoadMvvm.View> implements LoadM
         this.preferences = preferences;
         this.parser = parser;
         this.waypointsRepo = waypointsRepo;
+        this.context=context;
     }
 
     public void attachView(@Nullable Bundle savedInstanceState, @NonNull LoadMvvm.View view) {
         super.attachView(savedInstanceState, view);
     }
 
-    public void setConfiguration(String json) throws IOException, Parser.EncryptionException {
+    private void setConfiguration(String json) throws IOException, Parser.EncryptionException {
         MessageBase message = parser.fromJson(json.getBytes());
         if (message instanceof MessageConfiguration) {
             this.configuration = (MessageConfiguration) parser.fromJson(json.getBytes());
-            hasConfigurationLive.postValue(true);
             String prettyConfiguration;
             try {
                 prettyConfiguration = parser.toJsonPlainPretty(this.configuration);
@@ -63,6 +83,7 @@ public class LoadViewModel extends BaseViewModel<LoadMvvm.View> implements LoadM
         }
     }
 
+    @Override
     public void saveConfiguration() {
         preferences.importFromMessage(configuration);
         if (!configuration.getWaypoints().isEmpty()) {
@@ -73,12 +94,81 @@ public class LoadViewModel extends BaseViewModel<LoadMvvm.View> implements LoadM
     }
 
     @Override
-    public MutableLiveData<Boolean> hasConfiguration() {
-        return hasConfigurationLive;
+    public MutableLiveData<String> formattedEffectiveConfiguration() {
+        return formattedEffectiveConfiguration;
     }
 
     @Override
-    public MutableLiveData<String> formattedEffectiveConfiguration() {
-        return formattedEffectiveConfiguration;
+    public MutableLiveData<Throwable> importFailure() {
+        return importFailure;
+    }
+
+    @Override
+    public void extractPreferences(Uri uri) {
+        try {
+            BufferedReader r;
+            if (ContentResolver.SCHEME_FILE.equals(uri.getScheme())) {
+                // Note: left here to avoid breaking compatibility.  May be removed
+                // with sufficient testing. Will not work on Android >5 without granting READ_EXTERNAL_STORAGE permission
+                Timber.v("using file:// uri");
+                r = new BufferedReader(new InputStreamReader(new FileInputStream(uri.getPath())));
+            } else if ("owntracks".equals(uri.getScheme()) && "/config".equals(uri.getPath())) {
+                Timber.v("Importing config using owntracks: scheme");
+
+                List<String> urlQueryParam = uri.getQueryParameters("url");
+                List<String> configQueryParam = uri.getQueryParameters("inline");
+                if (configQueryParam.size() == 1) {
+                    byte[] config = Base64.decode(configQueryParam.get(0), Base64.DEFAULT);
+                    r = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(config)));
+                } else if (urlQueryParam.size() == 1) {
+                    URL remoteConfigUrl = new URL(urlQueryParam.get(0));
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url(remoteConfigUrl)
+                            .build();
+
+                    client.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            importFailure.postValue(new Exception("Failure fetching config from remote URL", e));
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            try (ResponseBody responseBody = response.body()) {
+                                if (!response.isSuccessful()) {
+                                    importFailure.postValue(new IOException(String.format("Unexpected status code: %s", response)));
+                                    return;
+                                }
+                                Thread.sleep(5000);
+                                setConfiguration(responseBody.string());
+                            } catch (Parser.EncryptionException | InterruptedException e) {
+                                importFailure.postValue(e);
+                            }
+                        }
+                    });
+                    // This is async, so result handled on the callback
+                    return;
+                } else {
+                    throw new IOException("Invalid config URL");
+                }
+            } else if ("content".equals(uri.getScheme())) {
+                Timber.v("using content:// uri");
+                InputStream stream = this.context.getContentResolver().openInputStream(uri);
+                r = new BufferedReader(new InputStreamReader(stream));
+            } else {
+                throw new IOException("Invalid config URL");
+            }
+
+            StringBuilder total = new StringBuilder();
+
+            String content;
+            while ((content = r.readLine()) != null) {
+                total.append(content);
+            }
+            setConfiguration(total.toString());
+        } catch (OutOfMemoryError | IOException | Parser.EncryptionException e) {
+            importFailure.postValue(e);
+        }
     }
 }
