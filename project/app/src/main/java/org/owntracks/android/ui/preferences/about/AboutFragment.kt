fd2c7ac83bbc151990e0a0b0e7daaee2dc1026ab
Andrew Rowson <github@growse.com>
2021-11-23 21:59:19 +0000

    Added some new about screen string resources
    Also converted `aboutTranslationsSummary` to a plural. Partial fix for #1123

--- project/app/src/main/java/org/owntracks/android/ui/preferences/about/AboutFragment.kt
+++ project/app/src/main/java/org/owntracks/android/ui/preferences/about/AboutFragment.kt
@@ -33,7 +33,7 @@ class AboutFragment : PreferenceFragmentCompat() {
 
         findPreference<Preference>(UI_PREFERENCE_TRANSLATION)?.setSummaryProvider {
             val langCount = TRANSLATION_ARRAY.size
-            getString(R.string.aboutTranslationsSummary, langCount)
+            resources.getQuantityString(R.plurals.aboutTranslationsSummary, langCount)
         }
 
     }
