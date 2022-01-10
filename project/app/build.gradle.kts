10809979b2709e9e0a127efd8a3ba3ba00704fdc
Andrew Rowson <github@growse.com>
2022-01-10 11:28:28 +0000

    Bump google maps dependency to 18.0.1

--- project/app/build.gradle.kts
+++ project/app/build.gradle.kts
@@ -155,7 +155,7 @@ android {
         create("gms") {
             dimension = "locationProvider"
             dependencies {
-                gmsImplementation("com.google.android.gms:play-services-maps:17.0.1")
+                gmsImplementation("com.google.android.gms:play-services-maps:18.0.1")
                 gmsImplementation("com.google.android.gms:play-services-location:19.0.1")
             }
         }
