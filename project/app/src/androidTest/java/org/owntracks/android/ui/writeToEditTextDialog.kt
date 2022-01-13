filmgallery/android-app

Fix issue with ConnectionPreferences not correctly updating sharedPreferenceName on mode change

project/app/src/androidTest/java/org/owntracks/android/ui/writeToEditTextDialog.kt
@@ -1,0 +1,28 @@
+package org.owntracks.android.ui
+
+import android.R
+import androidx.annotation.StringRes
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import com.schibsted.spain.barista.interaction.BaristaDialogInteractions
+import com.schibsted.spain.barista.interaction.BaristaEditTextInteractions
+
+fun writeToEditTextDialog(@StringRes name: Int, value: String) {
+    scrollToText(name)
+    clickOnAndWait(name)
+    BaristaEditTextInteractions.writeTo(R.id.edit, value)
+    BaristaDialogInteractions.clickDialogPositiveButton()
+}
+
+fun scrollToText(textResource: Int) {
+    Espresso.onView(ViewMatchers.withId(androidx.preference.R.id.recycler_view))
+        .perform(
+            RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
+                ViewMatchers.hasDescendant(ViewMatchers.withText(textResource)),
+                ViewActions.scrollTo()
+            )
+        )
+}

project/app/src/androidTestOss/java/org/owntracks/android/ui/PreferencesActivityTests.kt
@@ -1,25 +1,19 @@
 package org.owntracks.android.ui
 
-import androidx.annotation.StringRes
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.scrollTo
-import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItem
-import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertContains
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertNotContains
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertNotExist
-import com.schibsted.spain.barista.interaction.BaristaDialogInteractions.clickDialogPositiveButton
-import com.schibsted.spain.barista.interaction.BaristaEditTextInteractions.writeTo
+import com.schibsted.spain.barista.interaction.BaristaSleepInteractions
 import com.schibsted.spain.barista.rule.flaky.AllowFlaky
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.owntracks.android.R
 import org.owntracks.android.TestWithAnActivity
 import org.owntracks.android.ui.preferences.PreferencesActivity
+import timber.log.Timber
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -106,6 +100,8 @@ class PreferencesActivityTests :
         clickBackAndWait()
         clickOnAndWait(R.string.configurationManagement)
 
+        BaristaSleepInteractions.sleep(1000)
+
         assertContains(R.id.effectiveConfiguration, "\"_type\" : \"configuration\"")
         assertContains(R.id.effectiveConfiguration, " \"waypoints\" : [ ]")
 
@@ -131,6 +127,7 @@ class PreferencesActivityTests :
 
         assertNotContains(R.id.effectiveConfiguration, "\"url\"")
         assertNotContains(R.id.effectiveConfiguration, "\"preferenceKeyDontReuseHttpClient\"")
+        Timber.e("parp")
     }
 
     @Test
@@ -145,7 +142,6 @@ class PreferencesActivityTests :
         writeToEditTextDialog(R.string.preferencesDeviceName, "testDeviceId")
         writeToEditTextDialog(R.string.preferencesTrackerId, "t1")
         clickBackAndWait()
-
 
         clickOnAndWait(R.string.preferencesReporting)
         clickOnAndWait(R.string.preferencesPubExtendedData)
@@ -213,21 +209,5 @@ class PreferencesActivityTests :
         clickOnAndWait(R.string.preferencesAdvanced)
         scrollToText(R.string.preferencesReverseGeocodeProvider)
         assertDisplayed(R.string.valDefaultGeocoder)
-    }
-
-    private fun scrollToText(textResource: Int) {
-        onView(withId(androidx.preference.R.id.recycler_view))
-            .perform(
-                actionOnItem<RecyclerView.ViewHolder>(
-                    hasDescendant(withText(textResource)), scrollTo()
-                )
-            )
-    }
-
-    private fun writeToEditTextDialog(@StringRes name: Int, value: String) {
-        scrollToText(name)
-        clickOnAndWait(name)
-        writeTo(android.R.id.edit, value)
-        clickDialogPositiveButton()
     }
 }

owntracks/android

project/app/src/androidTest/java/org/owntracks/android/ui/writeToEditTextDialog.kt
@@ -1,0 +1,28 @@
+package org.owntracks.android.ui
+
+import android.R
+import androidx.annotation.StringRes
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import com.adevinta.android.barista.interaction.BaristaDialogInteractions
+import com.adevinta.android.barista.interaction.BaristaEditTextInteractions
+
+fun writeToEditTextDialog(@StringRes name: Int, value: String) {
+    scrollToText(name)
+    clickOnAndWait(name)
+    BaristaEditTextInteractions.writeTo(R.id.edit, value)
+    BaristaDialogInteractions.clickDialogPositiveButton()
+}
+
+fun scrollToText(textResource: Int) {
+    Espresso.onView(ViewMatchers.withId(androidx.preference.R.id.recycler_view))
+        .perform(
+            RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
+                ViewMatchers.hasDescendant(ViewMatchers.withText(textResource)),
+                ViewActions.scrollTo()
+            )
+        )
+}

project/app/src/androidTestOss/java/org/owntracks/android/ui/PreferencesActivityTests.kt
@@ -1,19 +1,12 @@
 package org.owntracks.android.ui
 
-import androidx.annotation.StringRes
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.scrollTo
-import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItem
-import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertContains
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertNotContains
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertNotExist
-import com.adevinta.android.barista.interaction.BaristaDialogInteractions.clickDialogPositiveButton
-import com.adevinta.android.barista.interaction.BaristaEditTextInteractions.writeTo
+import com.adevinta.android.barista.interaction.BaristaSleepInteractions
 import com.adevinta.android.barista.rule.flaky.AllowFlaky
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -106,6 +99,8 @@ class PreferencesActivityTests :
         clickBackAndWait()
         clickOnAndWait(R.string.configurationManagement)
 
+        BaristaSleepInteractions.sleep(1000)
+
         assertContains(R.id.effectiveConfiguration, "\"_type\" : \"configuration\"")
         assertContains(R.id.effectiveConfiguration, " \"waypoints\" : [ ]")
 
@@ -145,7 +140,6 @@ class PreferencesActivityTests :
         writeToEditTextDialog(R.string.preferencesDeviceName, "testDeviceId")
         writeToEditTextDialog(R.string.preferencesTrackerId, "t1")
         clickBackAndWait()
-
 
         clickOnAndWait(R.string.preferencesReporting)
         clickOnAndWait(R.string.preferencesPubExtendedData)
@@ -213,21 +207,5 @@ class PreferencesActivityTests :
         clickOnAndWait(R.string.preferencesAdvanced)
         scrollToText(R.string.preferencesReverseGeocodeProvider)
         assertDisplayed(R.string.valDefaultGeocoder)
-    }
-
-    private fun scrollToText(textResource: Int) {
-        onView(withId(androidx.preference.R.id.recycler_view))
-            .perform(
-                actionOnItem<RecyclerView.ViewHolder>(
-                    hasDescendant(withText(textResource)), scrollTo()
-                )
-            )
-    }
-
-    private fun writeToEditTextDialog(@StringRes name: Int, value: String) {
-        scrollToText(name)
-        clickOnAndWait(name)
-        writeTo(android.R.id.edit, value)
-        clickDialogPositiveButton()
     }
 }