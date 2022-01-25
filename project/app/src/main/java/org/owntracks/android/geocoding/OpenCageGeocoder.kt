d7bdda419bb8ff6a08ccee5bf171bad78e6d227b
Andrew Rowson <github@growse.com>
2022-01-25 11:59:56 +0000

    Opencage geocoder should log full response on error

--- project/app/src/main/java/org/owntracks/android/geocoding/OpenCageGeocoder.kt
+++ project/app/src/main/java/org/owntracks/android/geocoding/OpenCageGeocoder.kt
@@ -50,65 +50,66 @@ class OpenCageGeocoder @JvmOverloads internal constructor(
         return try {
             httpClient.newCall(request).execute().use { response ->
                 val responseBody = response.body?.string()
-                when (response.code) {
-                    200 -> {
-                        val deserializedOpenCageResponse =
-                            jsonMapper.readValue(responseBody, OpenCageResponse::class.java)
-                        Timber.d("Opencage HTTP response: %s", responseBody)
-                        return deserializedOpenCageResponse.formatted?.let {
-                            GeocodeResult.Formatted(
-                                it
+                try {
+                    when (response.code) {
+                        200 -> {
+                            val deserializedOpenCageResponse =
+                                jsonMapper.readValue(responseBody, OpenCageResponse::class.java)
+                            Timber.d("Opencage HTTP response: %s", responseBody)
+                            return deserializedOpenCageResponse.formatted?.let {
+                                GeocodeResult.Formatted(it)
+                            } ?: GeocodeResult.Empty
+                        }
+                        401 -> {
+                            val deserializedOpenCageResponse =
+                                jsonMapper.readValue(responseBody, OpenCageResponse::class.java)
+                            tripResetTimestamp = Instant.now().plus(1, ChronoUnit.MINUTES)
+                            GeocodeResult.Error(
+                                deserializedOpenCageResponse.status?.message
+                                    ?: "No error message provided", tripResetTimestamp
                             )
                         }
-                            ?: GeocodeResult.Empty
-                    }
-                    401 -> {
-                        val deserializedOpenCageResponse =
-                            jsonMapper.readValue(responseBody, OpenCageResponse::class.java)
-                        tripResetTimestamp = Instant.now().plus(1, ChronoUnit.MINUTES)
-                        GeocodeResult.Error(
-                            deserializedOpenCageResponse.status?.message
-                                ?: "No error message provided", tripResetTimestamp
-                        )
-                    }
-                    402 -> {
-                        val deserializedOpenCageResponse =
-                            jsonMapper.readValue(responseBody, OpenCageResponse::class.java)
-                        Timber.d("Opencage HTTP response: %s", responseBody)
-                        Timber.w("Opencage quota exceeded")
-                        deserializedOpenCageResponse.rate?.let { rate ->
-                            Timber.w("Not retrying Opencage requests until ${rate.reset}")
-                            tripResetTimestamp = rate.reset
+                        402 -> {
+                            val deserializedOpenCageResponse =
+                                jsonMapper.readValue(responseBody, OpenCageResponse::class.java)
+                            Timber.d("Opencage HTTP response: %s", responseBody)
+                            Timber.w("Opencage quota exceeded")
+                            deserializedOpenCageResponse.rate?.let { rate ->
+                                Timber.w("Not retrying Opencage requests until ${rate.reset}")
+                                tripResetTimestamp = rate.reset
+                            }
+                            GeocodeResult.RateLimited(tripResetTimestamp)
                         }
-                        GeocodeResult.RateLimited(tripResetTimestamp)
-                    }
-                    403 -> {
-                        val deserializedOpenCageResponse =
-                            jsonMapper.readValue(responseBody, OpenCageResponse::class.java)
-                        Timber.e(responseBody)
-                        tripResetTimestamp = Instant.now().plus(1, ChronoUnit.MINUTES)
-                        if (deserializedOpenCageResponse.status?.message == "IP address rejected") {
-                            GeocodeResult.IPAddressRejected(tripResetTimestamp)
-                        } else {
-                            GeocodeResult.Disabled(tripResetTimestamp)
-                        }
+                        403 -> {
+                            val deserializedOpenCageResponse =
+                                jsonMapper.readValue(responseBody, OpenCageResponse::class.java)
+                            Timber.e(responseBody)
+                            tripResetTimestamp = Instant.now().plus(1, ChronoUnit.MINUTES)
+                            if (deserializedOpenCageResponse.status?.message == "IP address rejected") {
+                                GeocodeResult.IPAddressRejected(tripResetTimestamp)
+                            } else {
+                                GeocodeResult.Disabled(tripResetTimestamp)
+                            }
 
+                        }
+                        429 -> {
+                            tripResetTimestamp = Instant.now().plus(1, ChronoUnit.MINUTES)
+                            GeocodeResult.RateLimited(tripResetTimestamp)
+                        }
+                        else -> {
+                            tripResetTimestamp = Instant.now().plus(1, ChronoUnit.MINUTES)
+                            Timber.e("Unexpected response from Opencage: %s", response)
+                            GeocodeResult.Error(
+                                "status: ${response.code} $responseBody",
+                                tripResetTimestamp
+                            )
+                        }
                     }
-                    429 -> {
-                        tripResetTimestamp = Instant.now().plus(1, ChronoUnit.MINUTES)
-                        GeocodeResult.RateLimited(tripResetTimestamp)
-                    }
-                    else -> {
-                        tripResetTimestamp = Instant.now().plus(1, ChronoUnit.MINUTES)
-                        Timber.e("Unexpected response from Opencage: %s", response)
-                        GeocodeResult.Error(
-                            "status: ${response.code} $responseBody",
-                            tripResetTimestamp
-                        )
-                    }
+                } catch (e: Exception) {
+                    Timber.d("Json response: $responseBody")
+                    throw e
                 }
             }
-
         } catch (e: Exception) {
             tripResetTimestamp = Instant.now().plus(1, ChronoUnit.MINUTES)
             Timber.e(e, "Error reverse geocoding from opencage")
