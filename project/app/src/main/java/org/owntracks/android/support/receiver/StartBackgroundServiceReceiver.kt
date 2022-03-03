ac3e61b8b9030bfef5159aa501e4b74562a467cb
Andrew Rowson <github@growse.com>
2022-03-03 11:20:10 +0000

    Catch exception when starting service on boot or upgrade.

--- project/app/src/main/java/org/owntracks/android/support/receiver/StartBackgroundServiceReceiver.kt
+++ project/app/src/main/java/org/owntracks/android/support/receiver/StartBackgroundServiceReceiver.kt
@@ -1,5 +1,6 @@
 package org.owntracks.android.support.receiver
 
+import android.app.ForegroundServiceStartNotAllowedException
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -17,8 +18,8 @@ class StartBackgroundServiceReceiver : BroadcastReceiver() {
     lateinit var preferences: Preferences
 
     override fun onReceive(context: Context, intent: Intent) {
-        if (("android.intent.action.MY_PACKAGE_REPLACED" == intent.action ||
-                    "android.intent.action.BOOT_COMPLETED" == intent.action)
+        if ((Intent.ACTION_MY_PACKAGE_REPLACED == intent.action ||
+                    Intent.ACTION_BOOT_COMPLETED == intent.action)
             && preferences.autostartOnBoot
         ) {
             Timber.v("android.intent.action.BOOT_COMPLETED received")
@@ -26,7 +27,11 @@ class StartBackgroundServiceReceiver : BroadcastReceiver() {
                 Timber.v("running startForegroundService")
                 val startIntent = Intent(context, BackgroundService::class.java)
                 startIntent.action = intent.action
-                context.startForegroundService(startIntent)
+                try {
+                    context.startForegroundService(startIntent)
+                } catch (e: ForegroundServiceStartNotAllowedException) {
+                    Timber.e("Unable to start foreground service, because Android has prevented it. This should not happen if intent action is ${Intent.ACTION_MY_PACKAGE_REPLACED} or ${Intent.ACTION_BOOT_COMPLETED}. intent action was ${intent.action}")
+                }
             } else {
                 Timber.v("running legacy startService")
                 context.startService(Intent(context, BackgroundService::class.java))
