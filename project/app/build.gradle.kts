b2f54b50e553aad463b461023e68c135ade2b960
Andrew Rowson <github@growse.com>
2022-03-14 13:21:39 +0000

    Bump lifecycle-common-java8 to 2.4.1

--- project/app/build.gradle.kts
+++ project/app/build.gradle.kts
@@ -201,7 +201,7 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.4.1")
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.test.espresso:espresso-idling-resource:${espressoVersion}")
-    implementation("androidx.lifecycle:lifecycle-common-java8:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.4.1")
 
     implementation("com.google.android.material:material:1.5.0")
 
