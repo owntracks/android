2aac70854c15d8f9c73abda18f8efa67e51aab5a
Andrew Rowson <github@growse.com>
2023-01-30 14:10:38 +0000

    Updated local test utils to use docker compose stack

--- util/mqtt-local/generate-certs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-mkcert -client -pkcs12 test
-mkcert 10.0.2.2
-chmod 444 *.pem
--- util/mqtt-local/start-local-mosquitto.sh
+++ util/mqtt-local/start-local-mosquitto.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
- docker run --rm --name mosquitto \
-	 -p 1883:1883 \
-	 -p 8883:8883 \
-	 -p 8080:8080 \
-	 -v $(pwd)/mosquitto.conf:/mosquitto/config/mosquitto.conf:ro \
-	 -v $(pwd)/mosquitto.password:/mosquitto/config/mosquitto.password:ro \
-	 -v $(pwd)/rootCA.pem:/mosquitto/config/rootCA.pem:ro \
-	 -v $(pwd)/_wildcard.docker.local-key.pem:/mosquitto/config/key.pem:ro \
-	 -v $(pwd)/_wildcard.docker.local.pem:/mosquitto/config/cert.pem:ro \
-	 eclipse-mosquitto:2.0.15-openssl
+docker network create owntracks-network || echo "Netork already exists"
+docker run --rm --name mosquitto \
+	-p 1883:1883 \
+	-p 8883:8883 \
+	-p 8080:8080 \
+	-v $(pwd)/mosquitto.conf:/mosquitto/config/mosquitto.conf:ro \
+	-v $(pwd)/mosquitto.password:/mosquitto/config/mosquitto.password:ro \
+	-v $(pwd)/rootCA.pem:/mosquitto/config/rootCA.pem:ro \
+	-v $(pwd)/_wildcard.docker.local-key.pem:/mosquitto/config/key.pem:ro \
+	-v $(pwd)/_wildcard.docker.local.pem:/mosquitto/config/cert.pem:ro \
+	--network="owntracks-network" \
+	eclipse-mosquitto:2.0.15-openssl
