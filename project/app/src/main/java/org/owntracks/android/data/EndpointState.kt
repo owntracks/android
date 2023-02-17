9199bb67b126fb63733ee7ca0a9b431b4d7b9d3f
Andrew Rowson <github@growse.com>
2023-02-17 22:14:13 +0000

    Status activity should properly localize the endpoint state message

--- project/app/src/main/java/org/owntracks/android/data/EndpointState.kt
+++ project/app/src/main/java/org/owntracks/android/data/EndpointState.kt
@@ -1,32 +1,16 @@
 package org.owntracks.android.data
 
 import android.content.Context
-import org.eclipse.paho.client.mqttv3.MqttException
 import org.owntracks.android.R
-import java.util.*
 
 enum class EndpointState {
     INITIAL, IDLE, CONNECTING, CONNECTED, DISCONNECTED, ERROR, ERROR_DATADISABLED, ERROR_CONFIGURATION;
 
     var message: String? = null
-        get() {
-            if (field == null) {
-                return error?.let {
-                    if (it is MqttException && it.cause != null) {
-                        if (it.message != null && it.message == "MqttException") {
-                            String.format("MQTT Error: %s", it.cause!!.message)
-                        } else String.format("MQTT Error: %s", it.message)
-                    } else error!!.message
-                }
-            } else if (error != null) {
-                return String.format(Locale.ROOT, "%s: %s", field, error!!.message)
-            }
-            return field
-        }
     var error: Throwable? = null
         private set
 
-    fun withMessage(message: String?): EndpointState {
+    fun withMessage(message: String): EndpointState {
         this.message = message
         return this
     }
--- project/app/src/main/java/org/owntracks/android/ui/status/StatusActivity.kt
+++ project/app/src/main/java/org/owntracks/android/ui/status/StatusActivity.kt
@@ -2,15 +2,28 @@ package org.owntracks.android.ui.status
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
+import java.net.SocketTimeoutException
+import java.net.URISyntaxException
+import java.net.UnknownHostException
 import javax.inject.Inject
+import javax.net.ssl.SSLException
+import org.eclipse.paho.client.mqttv3.MqttException
+import org.eclipse.paho.client.mqttv3.MqttException.*
 import org.owntracks.android.R
+import org.owntracks.android.data.EndpointState
 import org.owntracks.android.databinding.UiStatusBinding
+import org.owntracks.android.services.MqttConnectionConfiguration
 import org.owntracks.android.support.DrawerProvider
+import org.owntracks.android.support.interfaces.ConfigurationIncompleteException
 import org.owntracks.android.ui.status.logs.LogViewerActivity
 
 @AndroidEntryPoint
@@ -58,5 +71,78 @@ class StatusActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         viewModel.refreshDozeModeWhitelisted()
+    }
+
+    companion object {
+        @JvmStatic
+        @BindingAdapter("app:endpointState")
+        fun setText(view: TextView, endpointState: EndpointState) {
+            view.text = if (endpointState.error != null) {
+                when (val e = endpointState.error) {
+                    is ConfigurationIncompleteException -> {
+                        when (e.cause) {
+                            is MqttConnectionConfiguration.MissingHostException -> view.context.getString(
+                                R.string.statusEndpointStateMessageMissingHost
+                            )
+                            is URISyntaxException -> view.context.getString(
+                                R.string.statusEndpointStateMessageMalformedHostPort
+                            )
+                            else -> e.toString()
+                        }
+                    }
+                    is MqttException -> when (val mqttException = e.cause) {
+                        is UnknownHostException -> view.context.getString(
+                            R.string.statusEndpointStateMessageUnknownHost
+                        )
+                        is SocketTimeoutException -> view.context.getString(
+                            R.string.statusEndpointStateMessageSocketTimeout
+                        )
+                        is SSLException -> view.context.getString(
+                            R.string.statusEndpointStateMessageTLSError,
+                            mqttException.message
+                        )
+                        else -> when (e.reasonCode.toShort()) {
+                            REASON_CODE_INVALID_PROTOCOL_VERSION -> view.context.getString(
+                                R.string.statusEndpointStateMessageInvalidProtocolVersion
+                            )
+                            REASON_CODE_INVALID_CLIENT_ID -> view.context.getString(
+                                R.string.statusEndpointStateMessageInvalidClientId
+                            )
+                            REASON_CODE_FAILED_AUTHENTICATION -> view.context.getString(
+                                R.string.statusEndpointStateMessageAuthenticationFailed
+                            )
+                            REASON_CODE_NOT_AUTHORIZED -> view.context.getString(
+                                R.string.statusEndpointStateMessageNotAuthorized
+                            )
+                            REASON_CODE_SUBSCRIBE_FAILED -> view.context.getString(
+                                R.string.statusEndpointStateMessageSubscribeFailed
+                            )
+                            REASON_CODE_CLIENT_TIMEOUT -> view.context.getString(
+                                R.string.statusEndpointStateMessageClientTimeout
+                            )
+                            REASON_CODE_WRITE_TIMEOUT -> view.context.getString(
+                                R.string.statusEndpointStateMessageServerTimeout
+                            )
+                            REASON_CODE_SERVER_CONNECT_ERROR -> view.context.getString(
+                                R.string.statusEndpointStateMessageUnableToConnect
+                            )
+                            REASON_CODE_SSL_CONFIG_ERROR -> view.context.getString(
+                                R.string.statusEndpointStateMessageTLSConfigError
+                            )
+                            else -> e.toString()
+                        }
+                    }
+                    else -> e.toString()
+                }
+            } else {
+                endpointState.message
+            }
+        }
+
+        @JvmStatic
+        @BindingAdapter("app:endpointState")
+        fun setVisibility(view: LinearLayout, endpointState: EndpointState) {
+            view.isVisible = !(endpointState.error == null && endpointState.message == null)
+        }
     }
 }
