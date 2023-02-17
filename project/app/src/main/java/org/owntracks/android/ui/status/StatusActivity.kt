50d9b700d42168dafda31008fd89f1f6b06744d8
Andrew Rowson <github@growse.com>
2023-02-17 17:33:45 +0000

    Status activity should launch logs from activity itself

--- project/app/src/main/java/org/owntracks/android/ui/status/StatusActivity.kt
+++ project/app/src/main/java/org/owntracks/android/ui/status/StatusActivity.kt
@@ -1,15 +1,17 @@
 package org.owntracks.android.ui.status
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 import org.owntracks.android.R
 import org.owntracks.android.databinding.UiStatusBinding
 import org.owntracks.android.support.DrawerProvider
-import javax.inject.Inject
+import org.owntracks.android.ui.status.logs.LogViewerActivity
 
 @AndroidEntryPoint
 class StatusActivity : AppCompatActivity() {
@@ -19,26 +21,38 @@ class StatusActivity : AppCompatActivity() {
     private val batteryOptimizationIntents by lazy { BatteryOptimizingIntents(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: UiStatusBinding = DataBindingUtil.setContentView(this, R.layout.ui_status)
-        binding.vm = viewModel
-        binding.lifecycleOwner = this
-        setSupportActionBar(binding.appbar.toolbar)
-        drawerProvider.attach(binding.appbar.toolbar)
-        binding.dozeWhiteListed.setOnClickListener {
-            MaterialAlertDialogBuilder(this)
-                .setIcon(R.drawable.ic_baseline_battery_charging_full_24)
-                .setTitle(getString(R.string.batteryOptimizationWhitelistDialogTitle))
-                .setMessage(getString(R.string.batteryOptimizationWhitelistDialogMessage))
-                .setCancelable(true)
-                .setPositiveButton(getString(R.string.batteryOptimizationWhitelistDialogButtonLabel)) { _, _ ->
-                    if (viewModel.dozeWhitelisted.value == true) {
-                        startActivity(batteryOptimizationIntents.settingsIntent)
-                    } else {
-                        startActivity(batteryOptimizationIntents.directPackageIntent)
-                    }
+        DataBindingUtil.setContentView<UiStatusBinding>(this, R.layout.ui_status)
+            .apply {
+                vm = viewModel
+                lifecycleOwner = this@StatusActivity
+                appbar.toolbar.apply {
+                    setSupportActionBar(this)
+                    drawerProvider.attach(this)
                 }
-                .show()
-        }
+                dozeWhiteListed.setOnClickListener {
+                    MaterialAlertDialogBuilder(this@StatusActivity)
+                        .setIcon(R.drawable.ic_baseline_battery_charging_full_24)
+                        .setTitle(getString(R.string.batteryOptimizationWhitelistDialogTitle))
+                        .setMessage(getString(R.string.batteryOptimizationWhitelistDialogMessage))
+                        .setCancelable(true)
+                        .setPositiveButton(getString(R.string.batteryOptimizationWhitelistDialogButtonLabel)) { _, _ ->
+                            if (viewModel.dozeWhitelisted.value == true) {
+                                startActivity(batteryOptimizationIntents.settingsIntent)
+                            } else {
+                                startActivity(batteryOptimizationIntents.directPackageIntent)
+                            }
+                        }
+                        .show()
+                }
+                viewLogsButton.setOnClickListener {
+                    startActivity(
+                        Intent(
+                            this@StatusActivity,
+                            LogViewerActivity::class.java
+                        ).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    )
+                }
+            }
     }
 
     override fun onResume() {
--- project/app/src/main/java/org/owntracks/android/ui/status/StatusViewModel.kt
+++ project/app/src/main/java/org/owntracks/android/ui/status/StatusViewModel.kt
@@ -2,7 +2,6 @@ package org.owntracks.android.ui.status
 
 import android.app.Application
 import android.content.Context
-import android.content.Intent
 import android.location.Location
 import android.os.PowerManager
 import androidx.lifecycle.AndroidViewModel
@@ -14,7 +13,6 @@ import javax.inject.Inject
 import org.owntracks.android.data.EndpointState
 import org.owntracks.android.data.repos.EndpointStateRepo
 import org.owntracks.android.data.repos.LocationRepo
-import org.owntracks.android.ui.status.logs.LogViewerActivity
 
 @HiltViewModel
 class StatusViewModel @Inject constructor(
@@ -41,14 +39,5 @@ class StatusViewModel @Inject constructor(
         return powerManager.isIgnoringBatteryOptimizations(
             getApplication<Application>().applicationContext.packageName
         )
-    }
-
-    fun viewLogs() {
-        val intent =
-            Intent(
-                getApplication<Application>().applicationContext,
-                LogViewerActivity::class.java
-            ).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        getApplication<Application>().applicationContext.startActivity(intent)
     }
 }
