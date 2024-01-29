223d2934d4a2555550e4683475f350e74eae6d39
Andrew Rowson <github@growse.com>
2024-01-29 20:25:15 +0000

    Removed unnecessary imports

--- project/app/src/androidTestOss/java/org/owntracks/android/ui/map/OSSMapActivityTests.kt
+++ project/app/src/androidTestOss/java/org/owntracks/android/ui/map/OSSMapActivityTests.kt
@@ -1,6 +1,5 @@
 package org.owntracks.android.ui.map
 
-import android.Manifest.permission.ACCESS_FINE_LOCATION
 import androidx.preference.PreferenceManager
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -8,7 +7,6 @@ import androidx.test.platform.app.InstrumentationRegistry
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertNotExist
 import com.adevinta.android.barista.interaction.BaristaDialogInteractions.clickDialogNegativeButton
-import com.adevinta.android.barista.interaction.PermissionGranter
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.owntracks.android.R
