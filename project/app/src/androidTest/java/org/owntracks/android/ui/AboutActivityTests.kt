47816faeecf04702db9fdea86cf7c30f46d6581c
Andrew Rowson <github@growse.com>
2024-02-16 15:36:48 +0000

    AboutActivity tests should work with ATD image that doesn't have a browser installed

--- project/app/src/androidTest/java/org/owntracks/android/ui/AboutActivityTests.kt
+++ project/app/src/androidTest/java/org/owntracks/android/ui/AboutActivityTests.kt
@@ -1,11 +1,15 @@
 package org.owntracks.android.ui
 
+import android.app.Activity.RESULT_OK
+import android.app.Instrumentation
 import android.content.Intent
 import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import androidx.test.filters.SmallTest
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import com.adevinta.android.barista.interaction.BaristaClickInteractions.clickOn
 import org.hamcrest.CoreMatchers.allOf
@@ -16,43 +20,45 @@ import org.owntracks.android.testutils.TestWithAnActivity
 import org.owntracks.android.testutils.scrollToPreferenceWithText
 import org.owntracks.android.ui.preferences.about.AboutActivity
 
-@MediumTest
+@SmallTest
 @RunWith(AndroidJUnit4::class)
 class AboutActivityTests : TestWithAnActivity<AboutActivity>(AboutActivity::class.java) {
     @Test
     fun documentationLinkOpensSite() {
         scrollToPreferenceWithText(R.string.preferencesDocumentation)
-        clickOn(R.string.preferencesDocumentation)
-        intended(
-            allOf(
-                hasAction(Intent.ACTION_VIEW),
-                hasData(baristaRule.activityTestRule.activity.getString(R.string.documentationUrl))
-            )
+        val matcher = allOf(
+            hasAction(Intent.ACTION_VIEW),
+            hasData(baristaRule.activityTestRule.activity.getString(R.string.documentationUrl))
         )
+        intending(matcher).respondWith(Instrumentation.ActivityResult(RESULT_OK,null))
+        clickOn(R.string.preferencesDocumentation)
+        intended(matcher)
     }
 
     @Test
     fun sourceLinkOpensSite() {
         scrollToPreferenceWithText(R.string.preferencesRepository)
+        val matcher = allOf(
+            hasAction(Intent.ACTION_VIEW),
+            hasData(baristaRule.activityTestRule.activity.getString(R.string.repoUrl))
+        )
+        intending(matcher).respondWith(Instrumentation.ActivityResult(RESULT_OK,null))
         clickOn(R.string.preferencesRepository)
         intended(
-            allOf(
-                hasAction(Intent.ACTION_VIEW),
-                hasData(baristaRule.activityTestRule.activity.getString(R.string.repoUrl))
-            )
+            matcher
         )
     }
 
     @Test
     fun translationLinkOpensSite() {
         scrollToPreferenceWithText(R.string.aboutTranslations)
-        clickOn(R.string.aboutTranslations)
-        intended(
-            allOf(
-                hasAction(Intent.ACTION_VIEW),
-                hasData(baristaRule.activityTestRule.activity.getString(R.string.translationContributionUrl))
-            )
+        val matcher = allOf(
+            hasAction(Intent.ACTION_VIEW),
+            hasData(baristaRule.activityTestRule.activity.getString(R.string.translationContributionUrl))
         )
+        intending(matcher).respondWith(Instrumentation.ActivityResult(RESULT_OK,null))
+        clickOn(R.string.aboutTranslations)
+        intended(matcher)
     }
 
     @Test
