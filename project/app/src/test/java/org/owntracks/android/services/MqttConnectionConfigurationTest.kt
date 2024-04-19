51098e5db9706fbcd2df5b8af64bb2d4a4c1c7b3
wir3z <lpakula@velocitydevices.com>
2024-04-18 18:24:00 -0600

    Fixed unit test.

--- project/app/src/test/java/org/owntracks/android/services/MqttConnectionConfigurationTest.kt
+++ project/app/src/test/java/org/owntracks/android/services/MqttConnectionConfigurationTest.kt
@@ -21,6 +21,7 @@ class MqttConnectionConfigurationTest {
             "owntracks/+/+",
             "owntracks/+/+/event",
             "owntracks/+/+/info",
+            "owntracks/+/+/status",
             "owntracks/user/unknown/cmd"),
         topics)
   }
