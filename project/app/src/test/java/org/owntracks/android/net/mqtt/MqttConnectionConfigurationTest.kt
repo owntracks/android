512ad3a6b374761f68132a2e358e629352ba066c
wir3z <lpakula@velocitydevices.com>
2024-04-19 16:52:14 -0600

    MQTT unit tests

--- project/app/src/test/java/org/owntracks/android/net/mqtt/MqttConnectionConfigurationTest.kt
+++ project/app/src/test/java/org/owntracks/android/net/mqtt/MqttConnectionConfigurationTest.kt
@@ -20,7 +20,7 @@ class MqttConnectionConfigurationTest {
             "owntracks/+/+",
             "owntracks/+/+/event",
             "owntracks/+/+/info",
-//            "owntracks/+/+/status",
+            "owntracks/+/+/status",
             "owntracks/user/unknown/cmd"),
         topics)
   }
