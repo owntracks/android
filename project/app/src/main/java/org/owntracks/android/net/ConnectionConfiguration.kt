1b238914833593f97b6fc94f0da1295284393cf0
Felix Beckmann <diabloaushh@gmail.com>
2024-04-23 21:34:43 +0200

    SO_Timeout takes milliseconds not seconds
    also add exception debug logging

--- project/app/src/main/java/org/owntracks/android/net/ConnectionConfiguration.kt
+++ project/app/src/main/java/org/owntracks/android/net/ConnectionConfiguration.kt
@@ -3,6 +3,7 @@ package org.owntracks.android.net
 import android.content.Context
 import java.security.KeyStore
 import org.owntracks.android.support.SocketFactory
+import java.util.concurrent.TimeUnit
 
 interface ConnectionConfiguration {
   fun validate()
@@ -16,7 +17,7 @@ interface ConnectionConfiguration {
   ): SocketFactory =
       SocketFactory(
           SocketFactory.SocketFactoryOptions().apply {
-            socketTimeout = connectionTimeoutSeconds
+            socketTimeout = TimeUnit.SECONDS.toMillis(connectionTimeoutSeconds.toLong()).toInt()
             if (tls) {
               clientCertificateAlias = tlsClientCrt
             }
