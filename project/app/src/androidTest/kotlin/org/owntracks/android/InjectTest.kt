793faa9150a6fba33418cf585039ffcf0889eb52
Andrew Rowson <github@growse.com>
2025-01-08 10:29:22 +0000

    wip

--- project/app/src/androidTest/kotlin/org/owntracks/android/InjectTest.kt
+++ project/app/src/androidTest/kotlin/org/owntracks/android/InjectTest.kt
@@ -4,16 +4,24 @@ import android.app.Application
 import android.content.Context
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.runner.AndroidJUnitRunner
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.testing.CustomTestApplication
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.owntracks.android.di.LocationProviderClientModule
+import org.owntracks.android.location.LocationProviderClient
 import org.owntracks.android.testutils.JustThisTestPlease
 import org.owntracks.android.testutils.TestWithAnActivity
 import org.owntracks.android.ui.map.MapActivity
+import javax.inject.Singleton
 
 class CustomTestRunner : AndroidJUnitRunner() {
   override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
@@ -37,6 +45,21 @@ class InjectTest: TestWithAnActivity<MapActivity>(MapActivity::class.java,true) 
 
   @Test
   fun hiltInjectTest() {
-    // ...
+
   }
 }
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [LocationProviderClientModule::class]
+)
+abstract class FakeAnalyticsModule {
+  @Provides
+  @Singleton
+  fun getLocationProviderClient(
+    @ApplicationContext applicationContext: Context
+  ): LocationProviderClient = AospLocationProviderClient(applicationContext)
+}
+
+class Doubel
